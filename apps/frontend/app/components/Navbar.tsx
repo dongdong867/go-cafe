d97a3f5a576a8ef1509613a8cd3a3ee13c0088a9
dongdong867 <dongdong0867@gmail.com>
2023-04-13 05:05:44 +0800

    feat(navbar): set navbar with dropdown lists

---
+++ apps/frontend/app/components/Navbar.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+//packages
+import Image from 'next/image';
+import Link from 'next/link';
+
+//components
+import Logo from 'apps/frontend/public/images/logo.png';
+import Menu from './navbar/Menu';
+import Avatar from './navbar/Avatar';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar h-16 px-3">
+        <div className="navbar-start">
+          <Menu />
+        </div>
+        <div className="navbar-center">
+          <Link href={'/'}>
+            <Image src={Logo} alt="icon" className="w-12 h-12 aspect-auto" />
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <Avatar />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
