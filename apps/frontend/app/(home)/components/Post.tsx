ff3a1ea06fd8540445c0f602e3efda9f798c0a81
dongdong867 <dongdong0867@gmail.com>
2023-04-15 03:50:57 +0800

    feat(post modal): change link to collapse box

--- apps/frontend/app/(home)/components/Post.tsx
+++ apps/frontend/app/(home)/components/Post.tsx
@@ -6,7 +6,6 @@ import TemporaryImage from 'apps/frontend/public/images/logo.png';
 
 //icons
 import { FaStar } from 'react-icons/fa';
-import Link from 'next/link';
 
 type Props = {
   data: PostModalData;
@@ -15,42 +14,50 @@ type Props = {
 const Post = ({ data }: Props) => {
   return (
     <>
-      <Link href={`/post/${data.id}`}>
-        <div className="card w-full max-w-lg max-[450px]:max-w-[350px] h-[530px] max-[450px]:h-[450px] border-2 bg-base-200 mb-4">
-          {/* TODO: images need to be replace */}
-          <figure>
-            <div className="carousel bg-base-100">
-              <div className="carousel-item w-full">
-                <Image src={TemporaryImage} alt="" />
-              </div>
-              <div className="carousel-item w-full">
-                <Image src={TemporaryImage} alt="" />
-              </div>
-              <div className="carousel-item w-full">
-                <Image src={TemporaryImage} alt="" />
-              </div>
+      <div className="card w-full max-w-lg max-[450px]:max-w-[350px] h-max border-2 bg-base-200 mb-4">
+        {/* TODO: images need to be replace */}
+        <figure>
+          <div className="carousel bg-base-100">
+            <div className="carousel-item w-full">
+              <Image src={TemporaryImage} alt="" />
             </div>
-            {/* TODO: display image count function need to be handle */}
-            <div className="badge badge-primary text-white absolute top-2 right-2">
-              1/3
+            <div className="carousel-item w-full">
+              <Image src={TemporaryImage} alt="" />
             </div>
-          </figure>
-          <div className="card-body p-4">
-            <div className="flex items-center gap-x-1 text-base">
-              <FaStar className="text-accent" />
-              <div className="font-semibold">
-                {data.generalStars.toFixed(1)}
+            <div className="carousel-item w-full">
+              <Image src={TemporaryImage} alt="" />
+            </div>
+          </div>
+          {/* TODO: display image count function need to be handle */}
+          <div className="badge badge-primary text-white absolute top-2 right-2">
+            1/3
+          </div>
+        </figure>
+
+        <div className="card-body px-2 py-0">
+          <div className="collapse">
+            <input type="checkbox" />
+
+            <div className="collapse-title">
+              {/* stars */}
+              <div className="flex items-center gap-x-1 text-base">
+                <FaStar className="text-accent" />
+                <div className="font-semibold">
+                  {data.generalStars.toFixed(1)}
+                </div>
               </div>
-            </div>
-            <div>
+
+              {/* post title */}
               <div className="card-title text-xl font-bold">
                 {data.user.userName} @{data.coffeeShop.coffeeShopName}
               </div>
-              <div className="truncate font-medium">{data.body}</div>
             </div>
+
+            {/* post body */}
+            <div className="collapse-content font-medium">{data.body}</div>
           </div>
         </div>
-      </Link>
+      </div>
     </>
   );
 };
