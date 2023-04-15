9a3b7cfe66a4dac60d6e4fefaff814ae7955610d
dongdong867 <dongdong0867@gmail.com>
2023-04-16 07:36:34 +0800

    fix(home): fix post modal import route

--- apps/frontend/app/(home)/page.tsx
+++ apps/frontend/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 import SearchBar from '../components/SearchBar';
-import Post from './components/Post';
+import Post from '../components/Post';
 
 const Home = () => {
   const postList: PostModalData[] = [
