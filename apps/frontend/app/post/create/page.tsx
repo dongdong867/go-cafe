c003d33b3ef9948af700f9a2c3a92a53a78c3d6e
dongdong867 <dongdong0867@gmail.com>
2023-04-16 05:10:50 +0800

    feat(create post): create create post page

---
+++ apps/frontend/app/post/create/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditModal from '../components/EditModal';
+
+const CreatePostPage = () => {
+  return (
+    <>
+      <EditModal />
+    </>
+  );
+};
+
+export default CreatePostPage;
