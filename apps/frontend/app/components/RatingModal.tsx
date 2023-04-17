2bafd0f8026defbad06ec53aae84265b10ed8f99
dongdong867 <dongdong0867@gmail.com>
2023-04-18 05:47:06 +0800

    fix(rating modal): change  name, fix form error

--- apps/frontend/app/components/RatingModal.tsx
+++ apps/frontend/app/components/RatingModal.tsx
@@ -1,10 +1,20 @@
+'use client';
+
+import { useState } from 'react';
+
 type Props = {
   rateName: string;
+  rateValue: number;
   disable?: boolean;
   setRating?: React.Dispatch<React.SetStateAction<number>>;
 };
 
-const RankingModal = ({ rateName, disable = false, setRating }: Props) => {
+const RatingModal = ({
+  rateName,
+  rateValue,
+  disable = false,
+  setRating = () => {},
+}: Props) => {
   const handleClick = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
     setRating(parseInt((e.target as HTMLInputElement).value));
   };
@@ -12,51 +22,56 @@ const RankingModal = ({ rateName, disable = false, setRating }: Props) => {
     <>
       <div className="w-1/2 max-[450px]:w-5/6 flex justify-between">
         <div>{rateName}</div>
-        <div className="rating">
+        <form className="rating">
           <input
-            disabled={disable}
+            // disabled={disable}
             onClick={(e) => handleClick(e)}
             value={1}
             type="radio"
             name={rateName}
+            checked={rateValue === 1}
             className="mask mask-star-2 bg-accent"
           />
           <input
-            disabled={disable}
+            // disabled={disable}
             onClick={(e) => handleClick(e)}
             value={2}
             type="radio"
             name={rateName}
+            checked={rateValue === 2}
             className="mask mask-star-2 bg-accent"
           />
           <input
-            disabled={disable}
+            // disabled={disable}
             onClick={(e) => handleClick(e)}
             value={3}
             type="radio"
             name={rateName}
+            checked={rateValue === 3}
             className="mask mask-star-2 bg-accent"
           />
           <input
-            disabled={disable}
+            // disabled={disable}
             onClick={(e) => handleClick(e)}
             value={4}
             type="radio"
             name={rateName}
+            checked={rateValue === 4}
             className="mask mask-star-2 bg-accent"
           />
           <input
-            disabled={disable}
+            // disabled={disable}
             onClick={(e) => handleClick(e)}
             value={5}
             type="radio"
             name={rateName}
+            checked={rateValue === 5}
             className="mask mask-star-2 bg-accent"
           />
-        </div>
+        </form>
       </div>
     </>
   );
 };
 
-export default RankingModal;
+export default RatingModal;
