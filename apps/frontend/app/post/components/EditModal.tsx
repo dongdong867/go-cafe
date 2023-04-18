55cda5df9041432408f93367de800cc9f33e37e3
dongdong867 <dongdong0867@gmail.com>
2023-04-18 09:10:31 +0800

    feat(edit modal): move layout css to page

--- apps/frontend/app/post/components/EditModal.tsx
+++ apps/frontend/app/post/components/EditModal.tsx
@@ -5,27 +5,26 @@ import EditCoffeeShop from './EditModal/EditCoffeeShop';
 import EditRanking from './EditModal/EditRanking';
 import EditBody from './EditModal/EditBody';
 
-const EditModal = () => {
-  const buttonContent = 'Post';
+type Props = {
+  buttonContent: string;
+};
 
+const EditModal = ({ buttonContent }: Props) => {
   return (
     <>
-      <div className="w-full max-w-lg h-[calc(100vh-64px)] max-[450px]:max-w-[350px] m-auto overflow-scroll">
-        <div className=" text-base space-y-4">
-          <div className="space-y-4">
-            <EditPicture />
-            <EditCoffeeShop />
-            <EditRanking />
-          </div>
-          <div>
-            <EditBody />
-          </div>
-        </div>
-        <div className="w-full sticky bottom-0 bg-base-100 py-4">
-          <button className="btn btn-block btn-primary text-xl text-base-100">
-            {buttonContent}
-          </button>
-        </div>
+      <div className="text-lg font-medium space-y-4">
+        <EditPicture />
+        <EditCoffeeShop />
+        <EditRanking />
+        <EditBody />
+      </div>
+      <div className="w-full sticky bottom-0 bg-base-100 py-4">
+        <button
+          onClick={() => {}}
+          className="btn btn-block btn-primary text-xl text-base-100"
+        >
+          {buttonContent}
+        </button>
       </div>
     </>
   );
