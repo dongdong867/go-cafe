5a1fd3a4a3437b4bad67544cf28050772bd1064b
dongdong867 <dongdong0867@gmail.com>
2023-04-18 09:55:25 +0800

    feat(following): move fake data into public folder

--- apps/frontend/app/user/following/page.tsx
+++ apps/frontend/app/user/following/page.tsx
@@ -1,81 +1,14 @@
+import FollowingList from 'apps/frontend/public/data/FollowingList';
 import ShopCardModal from './components/ShopCardModal';
 
 const FollowingPage = () => {
-  const shopList: ShopCardForFollowingList[] = [
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-    {
-      id: '12345',
-      name: 'XXX Shop',
-      telephone: 1230487129384,
-      openingTime: '0800-2400',
-    },
-  ];
   return (
     <>
       <div className="w-full max-w-lg max-[450px]:w-11/12 m-auto pt-4">
         <div className="text-3xl font-bold">Following List</div>
         <div className="divider" />
         <div className="flex flex-col space-y-4">
-          {shopList.map((shop) => (
+          {FollowingList.map((shop) => (
             <ShopCardModal shop={shop} />
           ))}
         </div>
---
+++ apps/frontend/public/data/FollowingList.tsx
@@ -0,0 +1,70 @@
+const FollowingList: ShopCardForFollowingList[] = [
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+  {
+    id: '12345',
+    name: 'XXX Shop',
+    telephone: 1230487129384,
+    openingTime: '0800-2400',
+  },
+];
+
+export default FollowingList;
