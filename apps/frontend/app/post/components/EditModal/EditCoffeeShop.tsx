e657ef62bc8f4c119bdcd10ffad0d0a359855e1a
dongdong867 <dongdong0867@gmail.com>
2023-04-18 09:12:07 +0800

    style(edit modal): format css styles

--- apps/frontend/app/post/components/EditModal/EditCoffeeShop.tsx
+++ apps/frontend/app/post/components/EditModal/EditCoffeeShop.tsx
@@ -1,19 +1,19 @@
+'use client';
+
 import { MdLocationOn } from 'react-icons/md';
 
 const EditCoffeeShop = () => {
   return (
     <>
-      <div className="flex-none">
-        <label className="input-group">
-          <span>
-            <MdLocationOn className="text-2xl" />
-          </span>
-          <input
-            type="text"
-            placeholder="choose a coffee shop"
-            className="input input-bordered w-full focus:outline-none"
-          />
-        </label>
+      <div className="input-group">
+        <span>
+          <MdLocationOn className="text-2xl" />
+        </span>
+        <input
+          type="text"
+          placeholder="select a coffee shop"
+          className="input input-bordered w-full text-lg focus:outline-none"
+        />
       </div>
     </>
   );
