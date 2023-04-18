44ff9a59c1f3daa1385c665dd4a64c7647f0c9b7
dongdong867 <dongdong0867@gmail.com>
2023-04-18 09:13:46 +0800

    style(update modal): move layout css style to page

--- apps/frontend/app/post/update/[postId]/page.tsx
+++ apps/frontend/app/post/update/[postId]/page.tsx
@@ -3,7 +3,16 @@ import EditModal from '../../components/EditModal';
 const UpdatePostPage = () => {
   return (
     <>
-      <EditModal />
+      <div
+        className="
+          w-full max-w-lg 
+          h-[calc(100vh-64px)] 
+          max-[450px]:max-w-[350px] 
+          m-auto 
+          overflow-scroll"
+      >
+        <EditModal buttonContent="update" />
+      </div>
     </>
   );
 };
