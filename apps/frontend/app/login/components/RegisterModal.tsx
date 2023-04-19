309b00b1f89e130839131462f99f0af603c4f78d
dongdong867 <dongdong0867@gmail.com>
2023-04-20 05:34:45 +0800

    feat(sign up): rebuild by new modal and user info

--- apps/frontend/app/login/components/RegisterModal.tsx
+++ apps/frontend/app/login/components/RegisterModal.tsx
@@ -1,32 +1,38 @@
 'use client';
 
-import Modal from './Modal';
+import { useState } from 'react';
+import RegisterStepOne from './Register/RegisterStepOne';
+import RegisterStepTwo from './Register/RegisterStepTwo';
 
 type Props = {
   setSignin: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const RegisterModal = ({ setSignin }: Props) => {
-  const signupBody = (
-    <>
-      <div>Have have a account?</div>
-      <button
-        onClick={() => setSignin(true)}
-        className="text-[#78C2C4] underline-offset-4 hover:underline"
-      >
-        Sign in
-      </button>
-    </>
-  );
+  const [step, setStep] = useState(1);
+  const [account, setAccount] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
 
   return (
     <>
-      <Modal
-        title="Sign up"
-        disable={false}
-        onSubmit={() => {}}
-        switchContent={signupBody}
-      />
+      {step === 1 && (
+        <RegisterStepOne
+          setAccount={setAccount}
+          setPassword={setPassword}
+          setStep={setStep}
+          setSignin={setSignin}
+        />
+      )}
+      {step === 2 && (
+        <RegisterStepTwo
+          setName={setName}
+          setEmail={setEmail}
+          setPhone={setPhone}
+        />
+      )}
     </>
   );
 };
