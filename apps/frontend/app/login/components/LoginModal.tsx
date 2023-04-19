be8c68b35c7cc19aaa6918ec058a755f159cfe18
dongdong867 <dongdong0867@gmail.com>
2023-04-20 05:32:34 +0800

    feat(login modal): rebuild with new modal

--- apps/frontend/app/login/components/LoginModal.tsx
+++ apps/frontend/app/login/components/LoginModal.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { useState } from 'react';
 import Modal from './Modal';
 import { useRouter } from 'next/navigation';
+import InputModal from './InputModal';
 
 type Props = {
   setSignin: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,19 +12,33 @@ type Props = {
 const LoginModal = ({ setSignin }: Props) => {
   const router = useRouter();
 
-  const signinBody = (
-    <>
-      <div>Don't have a account?</div>
-      <button
-        onClick={() => setSignin(false)}
-        className="link link-primary underline-offset-4"
-      >
-        Sign up
-      </button>
-    </>
+  const [account, setAccount] = useState('');
+  const [password, setPassword] = useState('');
+
+  const bodyContent = (
+    <div className="space-y-4">
+      <div className="text-5xl max-[450px]:text-4xl font-bold mx-10">
+        Sign in
+      </div>
+      <div className=" grow mx-10 text-xl space-y-10 font-medium">
+        <div>
+          <InputModal labelText="ACCOUNT" setInput={setAccount} />
+          <InputModal labelText="PASSWORD" setInput={setPassword} />
+          <div className="flex gap-x-2 mt-2 text-base">
+            <div>Don't have a account?</div>
+            <button
+              onClick={() => setSignin(false)}
+              className="link link-primary underline-offset-4"
+            >
+              Sign up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 
-  const onSubmit = (account: string, password: string) => {
+  const onSubmit = () => {
     if (account === 'account' && password === 'password') {
       router.replace('/');
     } else {
@@ -32,12 +48,9 @@ const LoginModal = ({ setSignin }: Props) => {
 
   return (
     <>
-      <Modal
-        title="Sign in"
-        disable={false}
-        onSubmit={onSubmit}
-        switchContent={signinBody}
-      />
+      <Modal buttonContent="Sign in" onSubmit={onSubmit}>
+        {bodyContent}
+      </Modal>
     </>
   );
 };
