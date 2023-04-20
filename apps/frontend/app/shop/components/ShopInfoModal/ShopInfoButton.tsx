cb3741414fc3c15dd0c9727e1f09df20219c8ced
dongdong867 <dongdong0867@gmail.com>
2023-04-20 17:41:27 +0800

    feat(shop  info): add links and handle following

--- apps/frontend/app/shop/components/ShopInfoModal/ShopInfoButton.tsx
+++ apps/frontend/app/shop/components/ShopInfoModal/ShopInfoButton.tsx
@@ -1,14 +1,26 @@
 'use client';
 
+import Link from 'next/link';
+import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { FaHeart } from 'react-icons/fa';
 import { MdLocationOn, MdMenuBook } from 'react-icons/md';
 
 const ShopInfoButton = () => {
+  const param = useParams();
+  const router = useRouter();
+
+  const [followed, setFollowed] = useState(false);
+
+  const handleClick = () => {
+    setFollowed((e) => !e);
+  };
+
   return (
     <>
       <div className="card-actions justify-between">
-        <button
-          onClick={() => {}}
+        <Link
+          href={`/shop/${param.shopId}/menu`}
           className="
             btn 
             w-[31%] 
@@ -23,9 +35,9 @@ const ShopInfoButton = () => {
         >
           <MdMenuBook className="text-xl max-[450px]:text-lg" />
           Menu
-        </button>
-        <button
-          onClick={() => {}}
+        </Link>
+        <Link
+          href={`/shop/${param.shopId}/posts`}
           className="
             btn 
             w-[31%] 
@@ -40,20 +52,29 @@ const ShopInfoButton = () => {
         >
           <MdLocationOn className="text-xl max-[450px]:text-lg" />
           Posts
-        </button>
+        </Link>
         <button
-          onClick={() => {}}
-          className="
+          onClick={handleClick}
+          className={`
             btn 
             w-[32%] 
-            bg-base-100 hover:bg-secondary-focus
-            text-secondary-focus hover:text-base-100
             text-lg max-[450px]:text-sm 
             border-0 
             rounded-xl 
             flex 
             gap-x-2 
-            p-0"
+            p-0
+            ${
+              followed
+                ? 'bg-secondary-focus hover:bg-secondary-focus'
+                : 'bg-base-100 hover:bg-secondary-focus'
+            }
+            ${
+              followed
+                ? 'text-base-100 '
+                : 'text-secondary-focus hover:text-base-100'
+            }
+            `}
         >
           <FaHeart />
           Follow
