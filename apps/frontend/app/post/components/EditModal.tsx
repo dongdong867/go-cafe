94fe80fb2a0ffdd1150ca0efdaa910c5d7812278
dongdong867 <dongdong0867@gmail.com>
2023-04-20 16:15:49 +0800

    fea(edit modal): set init value for option prop

--- apps/frontend/app/post/components/EditModal.tsx
+++ apps/frontend/app/post/components/EditModal.tsx
@@ -8,42 +8,50 @@ import EditBody from './EditModal/EditBody';
 type Props = {
   buttonContent: string;
   // shop name
-  shopName: string;
-  shopNameDisabled: boolean;
+  shopName?: string;
+  shopNameDisabled?: boolean;
   setShopName: React.Dispatch<React.SetStateAction<string>>;
   // rating
-  generalRate: number;
-  environmentRate: number;
-  mealsRate: number;
-  attitudeRate: number;
+  generalRate?: number;
+  environmentRate?: number;
+  mealsRate?: number;
+  attitudeRate?: number;
   setGeneralRate: React.Dispatch<React.SetStateAction<number>>;
   setEnvironmentRate: React.Dispatch<React.SetStateAction<number>>;
   setMealsRate: React.Dispatch<React.SetStateAction<number>>;
   setAttitudeRate: React.Dispatch<React.SetStateAction<number>>;
   // post body
-  postBody: string;
+  postBody?: string;
   setPostBody: React.Dispatch<React.SetStateAction<string>>;
+  // submit
+  onSubmit: () => void;
 };
 
 const EditModal = ({
   buttonContent,
   // shop name
-  shopName,
-  shopNameDisabled,
+  shopName = '',
+  shopNameDisabled = false,
   setShopName,
   // rating
-  generalRate,
-  environmentRate,
-  mealsRate,
-  attitudeRate,
+  generalRate = 5,
+  environmentRate = 5,
+  mealsRate = 5,
+  attitudeRate = 5,
   setGeneralRate,
   setEnvironmentRate,
   setMealsRate,
   setAttitudeRate,
   // post body
-  postBody,
+  postBody = '',
   setPostBody,
+  // submit
+  onSubmit,
 }: Props) => {
+  const handleSubmit = () => {
+    onSubmit();
+  };
+
   return (
     <>
       <div className="text-lg font-medium space-y-4">
@@ -70,7 +78,7 @@ const EditModal = ({
       </div>
       <div className="w-full sticky bottom-0 bg-base-100 py-4">
         <button
-          onClick={() => {}}
+          onClick={handleSubmit}
           className="btn btn-block btn-primary text-xl text-base-100"
         >
           {buttonContent}
