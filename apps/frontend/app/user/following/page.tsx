1ae26d80541744d3c63b10319ddf53d6a4edd3e9
dongdong867 <dongdong0867@gmail.com>
2023-04-24 15:19:53 +0800

    feat(title): create page title component

--- apps/frontend/app/user/following/page.tsx
+++ apps/frontend/app/user/following/page.tsx
@@ -1,12 +1,12 @@
 import FollowingList from 'apps/frontend/public/data/FollowingList';
 import ShopCardModal from './components/ShopCardModal';
+import PageTitle from '../../components/PageTitle';
 
 const FollowingPage = () => {
   return (
     <>
-      <div className="w-full max-w-lg max-[450px]:w-11/12 m-auto pt-4">
-        <div className="text-3xl font-bold">Following List</div>
-        <div className="divider" />
+      <div className="w-full max-w-lg max-[450px]:w-11/12 m-auto">
+        <PageTitle title="Following List" />
         <div className="flex flex-col space-y-4">
           {FollowingList.map((shop) => (
             <ShopCardModal shop={shop} />
