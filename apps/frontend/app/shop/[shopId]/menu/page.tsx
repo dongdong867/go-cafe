8643bc42de463a29385855136f4132802783bc81
dongdong867 <dongdong0867@gmail.com>
2023-04-24 16:45:14 +0800

    feat(menu): create menu page for ordering

--- apps/frontend/app/shop/[shopId]/menu/page.tsx
+++ apps/frontend/app/shop/[shopId]/menu/page.tsx
@@ -1,33 +1,34 @@
-import TemporaryPicture from 'apps/frontend/public/images/logo.png';
-import Image from 'next/image';
+import PageTitle from 'apps/frontend/app/components/PageTitle';
+import MenuCategory from './components/MenuCategory';
+import categories from 'apps/frontend/public/data/FakeMenuData';
 
 const ShopMenuPage = () => {
   return (
     <>
-      <div className="w-full max-w-lg max-[450px]:w-11/12 m-auto space-y-4">
-        <div className="text-3xl font-bold">Menu</div>
-        <div className="divider"></div>
-        <div className="carousel rounded-box">
-          <div className="carousel-item w-full">
-            <Image
-              src={TemporaryPicture}
-              alt=""
-              className="w-full h-max aspect-auto"
-            />
-          </div>
-          <div className="carousel-item w-full aspect-auto">
-            <Image
-              src={TemporaryPicture}
-              alt=""
-              className="w-full h-max aspect-auto"
-            />
-          </div>
-          <div className="carousel-item w-full">
-            <Image
-              src={TemporaryPicture}
-              alt=""
-              className="w-full h-max aspect-auto"
-            />
+      <div className="w-full max-w-lg max-[450px]:w-11/12 m-auto space-y-4 relative">
+        <PageTitle title="Menu" />
+        <div className="btn-group w-full px-1 overflow-x-scroll">
+          {categories.map((category) => (
+            <a
+              key={category.title}
+              href={`#${category.title}`}
+              className="btn btn-primary text-white text-base"
+            >
+              {category.title}
+            </a>
+          ))}
+        </div>
+        <div className="menu bg-base-100 w-full h-full rounded-box px-4 py-8 shadow-[0_2px_15px_rgba(0,0,0,0.25)]">
+          {categories.map((category) => (
+            <MenuCategory key={category.title} category={category} />
+          ))}
+        </div>
+        <div className="w-0 h-20" />
+        <div className="fixed w-full bg-base-100 left-0 bottom-0">
+          <div className="w-11/12 max-w-lg m-auto py-4">
+            <button className="btn btn-primary btn-block text-white text-xl">
+              <span>check out</span>
+            </button>
           </div>
         </div>
       </div>
---
+++ apps/frontend/public/data/FakeMenuData.tsx
@@ -0,0 +1,20 @@
+const categories = [
+  {
+    title: 'coffee',
+    items: [{ name: 'america' }, { name: 'latte' }, { name: 'cappuccino' }],
+  },
+  {
+    title: 'tea',
+    items: [{ name: 'black tea' }, { name: 'green tea' }, { name: 'milk tea' }],
+  },
+  {
+    title: 'toast',
+    items: [
+      { name: 'apple toast' },
+      { name: 'banana toast' },
+      { name: 'guava toast' },
+    ],
+  },
+];
+
+export default categories;
