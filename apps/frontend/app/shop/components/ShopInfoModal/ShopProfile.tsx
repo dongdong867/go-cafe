846391a59b9a0bd8117509f5e5aea484340aec4c
dongdong867 <dongdong0867@gmail.com>
2023-04-24 16:44:43 +0800

    fix(shop page): fix shop info padding

--- apps/frontend/app/shop/components/ShopInfoModal/ShopProfile.tsx
+++ apps/frontend/app/shop/components/ShopInfoModal/ShopProfile.tsx
@@ -5,34 +5,34 @@ type Props = {
 const ShopProfile = ({ data }: Props) => {
   return (
     <>
-      <div className="bg-base-100 p-4 rounded-xl">
+      <div className="bg-base-100 p-4 rounded-xl space-y-4">
         <div className="card-title text-2xl font-bold">{data.name}</div>
         <div className="font-medium space-y-2">
           <div className="text-base">
-            <div className="flex">
+            <div className="flex space-x-1">
               <div>Tel: </div>
-              <div>{data.telephone}</div>
+              <div className="font-semibold">{data.telephone}</div>
             </div>
-            <div className="flex">
+            <div className="flex space-x-1">
               <div>Opening time: </div>
-              <div>{data.openingTime}</div>
+              <div className="font-semibold">{data.openingTime}</div>
             </div>
-          </div>
-          <div className="text-sm">
-            <div>Address: </div>
-            <div className="flex gap-x-1 flex-wrap">
-              <div className="shrink-0">{data.address.city} City</div>
-              <div className="shrink-0">{data.address.area} Area</div>
-              <div className="shrink-0">{data.address.road} Road</div>
-              {data.address.other && (
-                <div className="shrink-0">{data.address.other}</div>
-              )}
-              <div className="shrink-0">No. {data.address.houseNumber}</div>
-              {data.address.building && (
-                <div className="shrink-0">
-                  -{data.address.building.subHouseNumber}
-                </div>
-              )}
+            <div className="mt-4">
+              <div>Address: </div>
+              <div className="flex gap-x-1 flex-wrap font-semibold">
+                <div className="shrink-0">{data.address.city} City</div>
+                <div className="shrink-0">{data.address.area} Area</div>
+                <div className="shrink-0">{data.address.road} Road</div>
+                {data.address.other && (
+                  <div className="shrink-0">{data.address.other}</div>
+                )}
+                <div className="shrink-0">No. {data.address.houseNumber}</div>
+                {data.address.building && (
+                  <div className="shrink-0">
+                    -{data.address.building.subHouseNumber}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
