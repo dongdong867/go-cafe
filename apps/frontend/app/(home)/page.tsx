5b7592ae9ea025e9c48f33aefb731c65e46b300d
dongdong867 <dongdong0867@gmail.com>
2023-04-26 18:08:44 +0800

    style(search bar): move file to input folder

--- apps/frontend/app/(home)/page.tsx
+++ apps/frontend/app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import SearchBar from '../components/SearchBar';
+import SearchBar from '../components/Input/SearchBar';
 import UserPostModal from '../components/UserPostModal';
 import userPostList from 'apps/frontend/public/data/UserPostList';
 
