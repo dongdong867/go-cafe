52c2f67a93b63dd8d25e2e33fa6186c0a1667562
dongdong867 <dongdong0867@gmail.com>
2023-05-08 14:58:18 +0800

    feat(store r): create getStores query and args

--- apps/backend/src/user/store/store.resolver.ts
+++ apps/backend/src/user/store/store.resolver.ts
@@ -2,13 +2,19 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { StoreService } from './store.service';
 import { Store } from './entities/store.entity';
 import { GetStoreArgs } from './dto/args/get-store.args';
+import { GetStoresArgs } from './dto/args/get-stores.args';
 
 @Resolver(() => Store)
 export class StoreResolver {
   constructor(private readonly storeService: StoreService) {}
 
   @Query(() => Store, { name: 'store' })
-  getStore(@Args() getStoreArgs: GetStoreArgs) {
+  getStore(@Args() getStoreArgs: GetStoreArgs): Store {
     return new Store();
+  }
+
+  @Query(() => [Store], { name: 'stores' })
+  getStores(@Args() getStoresArgs: GetStoresArgs): Store[] {
+    return [new Store()];
   }
 }
