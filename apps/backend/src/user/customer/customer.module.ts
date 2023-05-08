89f2284ec7af08c34382c2b4eedb35a1b6ccc844
dongdong867 <dongdong0867@gmail.com>
2023-05-08 14:16:46 +0800

    feat(customer mo): add import of UserService

--- apps/backend/src/user/customer/customer.module.ts
+++ apps/backend/src/user/customer/customer.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerResolver } from './customer.resolver';
+import { UserService } from '../user.service';
 
 @Module({
-  providers: [CustomerResolver, CustomerService]
+  providers: [CustomerResolver, CustomerService, UserService],
 })
 export class CustomerModule {}
