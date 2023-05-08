62c14b9aa5c542754967a178a5535b61bab8a66d
dongdong867 <dongdong0867@gmail.com>
2023-05-08 21:27:44 +0800

    feat(following r): add service to resolver

--- apps/backend/src/app/following/following.resolver.ts
+++ apps/backend/src/app/following/following.resolver.ts
@@ -1,9 +1,12 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { FollowInput } from './dto/input/follow.input';
 import { UnfollowInput } from './dto/input/unfollow.input';
+import { FollowingService } from './following.service';
 
 @Resolver()
 export class FollowingResolver {
+  constructor(private readonly followingService: FollowingService) {}
+
   @Mutation(() => String)
   follow(@Args('followInput') followInput: FollowInput) {
     return 'Followed successfully';
