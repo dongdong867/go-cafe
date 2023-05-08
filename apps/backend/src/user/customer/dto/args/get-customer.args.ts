3b8b5b3e30e62d64a3b6ca4353a68c537e9fc60c
dongdong867 <dongdong0867@gmail.com>
2023-05-08 13:34:32 +0800

    fix(get customer): add missing account args

--- apps/backend/src/user/customer/dto/args/get-customer.args.ts
+++ apps/backend/src/user/customer/dto/args/get-customer.args.ts
@@ -4,6 +4,10 @@ import { IsNotEmpty, IsUUID } from 'class-validator';
 @ArgsType()
 export class GetCustomerArgs {
   @Field()
+  @IsNotEmpty()
+  account: string;
+
+  @Field()
   @IsUUID()
   @IsNotEmpty()
   token: string;
