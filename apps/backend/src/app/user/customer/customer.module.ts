defdc4276937dc145ba8776f032ba7e1cf07ae94
dongdong867 <dongdong0867@gmail.com>
2023-05-10 02:47:15 +0800

    feat(customer mo): add JwtModule setup

--- apps/backend/src/app/user/customer/customer.module.ts
+++ apps/backend/src/app/user/customer/customer.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerResolver } from './customer.resolver';
 import { UserService } from '../user.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   providers: [CustomerResolver, CustomerService, UserService],
 })
 export class CustomerModule {}
