dad2243906dc06fbac45a1d7b181865fe11828ce
dongdong867 <dongdong0867@gmail.com>
2023-05-10 02:54:49 +0800

    feat(customer s): update with current customer

--- apps/backend/src/app/user/customer/customer.service.ts
+++ apps/backend/src/app/user/customer/customer.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { Customer } from './models/customer.entity';
-import { GetCustomerArgs } from './dto/args/get-customer.args';
 import { UserService } from '../user.service';
 import { CreateCustomerInput } from './dto/inputs/create-customer.input';
 import { UpdateCustomerInput } from './dto/inputs/update-customer.input';
@@ -9,23 +8,7 @@ import { UpdateCustomerInput } from './dto/inputs/update-customer.input';
 export class CustomerService {
   constructor(private userService: UserService) {}
 
-  getCustomer(getCustomerArgs: GetCustomerArgs): Customer {
-    if (
-      !this.userService.certifyUser(
-        getCustomerArgs.account,
-        getCustomerArgs.token
-      )
-    )
-      throw new Error('invalidate user token');
-
-    const customer: Customer = {
-      name: 'test name',
-      phone: '0912345678',
-      postCount: 0,
-      followCount: 0,
-      email: 'test@example.com',
-      following: [],
-    };
+  getCustomer(customer: Customer): Customer {
     return customer;
   }
 
@@ -50,22 +33,13 @@ export class CustomerService {
     return customer;
   }
 
-  updateCustomer(updateCustomerInput: UpdateCustomerInput): Customer {
-    if (
-      !this.userService.certifyUser(
-        updateCustomerInput.account,
-        updateCustomerInput.token
-      )
-    )
-      throw new Error('invalidate user token');
-
-    const customer: Customer = {
-      name: updateCustomerInput.name || 'original name',
-      phone: updateCustomerInput.phone || 'original phone',
-      email: updateCustomerInput.email || 'original email',
-      followCount: 0,
-      postCount: 0,
-      following: [],
+  updateCustomer(
+    customer: Customer,
+    updateCustomerInput: UpdateCustomerInput
+  ): Customer {
+    customer = {
+      ...customer,
+      ...updateCustomerInput,
     };
 
     return customer;
