bd8970bd4b587400215265f0508254f601553f92
dongdong867 <dongdong0867@gmail.com>
2023-05-09 15:34:21 +0800

    feat(user mo): add JwtModule setup with secret

--- apps/backend/src/app/user/user.module.ts
+++ apps/backend/src/app/user/user.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { CustomerModule } from './customer/customer.module';
 import { StoreModule } from './store/store.module';
 import { UserService } from './user.service';
+import { UserResolver } from './user.resolver';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [CustomerModule, StoreModule],
+  imports: [
+    CustomerModule,
+    StoreModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_TOKEN,
+    }),
+  ],
   exports: [CustomerModule, StoreModule, UserService],
-  providers: [UserService],
+  providers: [UserService, UserResolver],
 })
 export class UserModule {}
