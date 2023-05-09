efe8fb9ca588a28ba21321a4d40bc6e512ff4eac
dongdong867 <dongdong0867@gmail.com>
2023-05-10 02:46:48 +0800

    feat(user s): add validateToken function

--- apps/backend/src/app/user/user.service.ts
+++ apps/backend/src/app/user/user.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Customer } from './customer/models/customer.entity';
+import { Store } from './store/models/store.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-  certifyUser(account: string, token: string): boolean {
-    return true;
+  constructor(private readonly jwtService: JwtService) {}
+  validateToken(token: string): {
+    isValidate: boolean;
+    user: Customer | Store;
+  } {
+    const decodedToken = this.jwtService.decode(token);
+
+    console.log(decodedToken);
+
+    // TODO: REPLACE FAKE DATA WITH DB DATA
+    //
+    const customer: Customer = {
+      name: 'test name',
+      phone: '0912345678',
+      postCount: 0,
+      followCount: 0,
+      email: 'test@example.com',
+      following: [],
+    };
+    return { isValidate: true, user: customer };
   }
 }
