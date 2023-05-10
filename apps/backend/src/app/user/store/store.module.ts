1b031ef120000b75c4930b1389d0ce3394da1bbe
dongdong867 <dongdong0867@gmail.com>
2023-05-11 03:04:07 +0800

    feat(store mo): add JwtModule setup

--- apps/backend/src/app/user/store/store.module.ts
+++ apps/backend/src/app/user/store/store.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { StoreResolver } from './store.resolver';
 import { UserService } from '../user.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   providers: [StoreResolver, StoreService, UserService],
 })
 export class StoreModule {}
