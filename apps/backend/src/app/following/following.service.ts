91b023d892a5b4048f8f99e01e5d74e3d28eff67
dongdong867 <dongdong0867@gmail.com>
2023-05-11 20:17:38 +0800

    feat(following s): rewrite un/follow function

--- apps/backend/src/app/following/following.service.ts
+++ apps/backend/src/app/following/following.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { FollowInput } from './dto/input/follow.input';
-import { UserService } from '../user/user.service';
-import { UnfollowInput } from './dto/input/unfollow.input';
+import { Customer } from '../user/customer/models/customer.entity';
 
 @Injectable()
 export class FollowingService {
-  constructor(private readonly userService: UserService) {}
-
-  follow(followInput: FollowInput): String {
-    if (!this.userService.certifyUser(followInput.account, followInput.token))
-      throw new Error('invalidate user token');
-
+  follow(user: Customer, followInput: FollowInput): String {
+    console.log('current user: ' + user.name);
     console.log('followed store account: ' + followInput.storeAccount);
     return 'Followed successfully';
   }
 
-  unfollow(unfollowInput: UnfollowInput) {
-    if (
-      !this.userService.certifyUser(unfollowInput.account, unfollowInput.token)
-    )
-      throw new Error('invalidate user token');
-
+  unfollow(user: Customer, unfollowInput: FollowInput) {
+    console.log('current user: ' + user.name);
     console.log('unfollowed store account: ' + unfollowInput.storeAccount);
     return 'Unfollowed successfully';
   }
