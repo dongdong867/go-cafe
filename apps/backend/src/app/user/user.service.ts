ea8cd41f240d2f0fd86e663e21f0b07a6551b471
dongdong867 <dongdong0867@gmail.com>
2023-05-11 19:41:14 +0800

    feat(user s): add login func and role validator

--- apps/backend/src/app/user/user.service.ts
+++ apps/backend/src/app/user/user.service.ts
@@ -1,28 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Customer } from './customer/models/customer.entity';
 import { Store } from './store/models/store.entity';
 import { JwtService } from '@nestjs/jwt';
+import { LoginInput } from './dto/input/login.input';
+
+type PayloadType = {
+  account: string;
+  role: string;
+  iat: number;
+};
 
 @Injectable()
 export class UserService {
   constructor(private readonly jwtService: JwtService) {}
+
+  login(loginInput: LoginInput) {
+    let role = '';
+    if (loginInput.account === 'test customer') role = 'customer';
+    else if (loginInput.account === 'test store') role = 'store';
+    else throw new BadRequestException('Login Failed');
+
+    const token = this.jwtService.sign({
+      account: loginInput.account,
+      role: role,
+    });
+
+    return token;
+  }
+
   validateToken(token: string): {
     isValidate: boolean;
     user: Customer | Store;
+    role: string;
   } {
-    const decodedToken = this.jwtService.decode(token);
-    console.log(decodedToken);
+    const decodedToken = this.jwtService.decode(token) as PayloadType;
 
-    // TODO: REPLACE FAKE DATA WITH DB DATA
-    //
-    const customer: Customer = {
-      name: 'test name',
-      phone: '0912345678',
-      postCount: 0,
-      followCount: 0,
-      email: 'test@example.com',
-      following: [],
-    };
-    return { isValidate: true, user: customer };
+    if (decodedToken.role === 'customer') {
+      // TODO: REPLACE FAKE DATA WITH DB DATA
+      //
+      const user: Customer = {
+        name: 'test customer',
+        phone: '0912345678',
+        postCount: 0,
+        followCount: 0,
+        email: 'customer@example.com',
+        following: [],
+      };
+      return { isValidate: true, user: user, role: 'customer' };
+    } else if (decodedToken.role === 'store') {
+      const user: Store = {
+        address: 'store address',
+        info: 'store info',
+        name: 'test store',
+        phone: '0912345678',
+        postCount: 0,
+      };
+      return { isValidate: true, user: user, role: 'store' };
+    }
   }
 }
