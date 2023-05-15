a42e43198e6c2ad2bccbcb206240a5a592f38e91
dongdong867 <dongdong0867@gmail.com>
2023-05-15 13:39:27 +0800

    feat(user-post mo): add imports of rating module

--- apps/backend/src/app/post/user-post/user-post.module.ts
+++ apps/backend/src/app/post/user-post/user-post.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserPostService } from './user-post.service';
 import { UserPostResolver } from './user-post.resolver';
 import { UserModule } from '../../user/user.module';
+import { RatingModule } from '../../rating/rating.module';
+import { RatingService } from '../../rating/rating.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, RatingModule],
   providers: [UserPostResolver, UserPostService],
 })
 export class UserPostModule {}
