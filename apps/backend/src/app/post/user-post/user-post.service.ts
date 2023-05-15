5ef1b4f31e1646cafaa9a8b787737e951f6008c1
dongdong867 <dongdong0867@gmail.com>
2023-05-15 13:40:40 +0800

    feat(user-post s): implement rating s and store s

--- apps/backend/src/app/post/user-post/user-post.service.ts
+++ apps/backend/src/app/post/user-post/user-post.service.ts
@@ -5,9 +5,16 @@ import { Customer } from '../../user/customer/models/customer.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { UpdateUserPostInput } from './dto/input/update-user-post.input';
 import { DeleteUserPostInput } from './dto/input/delete-user-post.input';
+import { RatingService } from '../../rating/rating.service';
+import { StoreService } from '../../user/store/store.service';
 
 @Injectable()
 export class UserPostService {
+  constructor(
+    private readonly ratingService: RatingService,
+    private readonly storeService: StoreService
+  ) {}
+
   getPosts(userId: string): UserPost[] {
     return [
       {
@@ -59,6 +66,11 @@ export class UserPostService {
       ...createUserPostInput,
     };
 
+    this.ratingService.addRating(
+      this.storeService.getStoreIdByAccount(createUserPostInput.storeAccount),
+      createUserPostInput.rating
+    );
+
     return post;
   }
 
@@ -66,19 +78,55 @@ export class UserPostService {
     currentUser: Customer,
     updateUserPostInput: UpdateUserPostInput
   ): UserPost {
-    const post: UserPost = {
+    const originPost: UserPost = {
+      id: 'postId1',
+      userAccount: 'user 1',
+      storeAccount: 'store 1',
+      body: 'this is a fake body',
+      rating: {
+        general: 4,
+        environment: 3,
+        meals: 3,
+        attitude: 2,
+      },
+    };
+    const newPost: UserPost = {
       userAccount: currentUser.account,
       storeAccount: 'test update store account',
       ...updateUserPostInput,
     };
 
-    return post;
+    const storeId = this.storeService.getStoreIdByAccount(
+      updateUserPostInput.storeAccount
+    );
+    this.ratingService.deleteRating(storeId, originPost.rating);
+    this.ratingService.addRating(storeId, updateUserPostInput.rating);
+
+    return newPost;
   }
 
   deleteUserPost(
     currentUser: Customer,
     deleteUserPostInput: DeleteUserPostInput
   ): string {
+    const originPost: UserPost = {
+      id: 'postId1',
+      userAccount: 'user 1',
+      storeAccount: 'store 1',
+      body: 'this is a fake body',
+      rating: {
+        general: 4,
+        environment: 3,
+        meals: 3,
+        attitude: 2,
+      },
+    };
+
+    this.ratingService.deleteRating(
+      this.storeService.getStoreIdByAccount(originPost.storeAccount),
+      originPost.rating
+    );
+
     return `delete post id: ${deleteUserPostInput.postId} successful`;
   }
 }
