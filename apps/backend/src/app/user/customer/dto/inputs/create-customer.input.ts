3dec9f9248280b19c71d0b9e32dce040c79b9542
dongdong867 <dongdong0867@gmail.com>
2023-05-18 03:23:06 +0800

    feat(create customer): add avatar field

--- apps/backend/src/app/user/customer/dto/inputs/create-customer.input.ts
+++ apps/backend/src/app/user/customer/dto/inputs/create-customer.input.ts
@@ -1,26 +1,22 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsNotEmpty, IsEmail } from 'class-validator';
 
 @InputType()
 export class CreateCustomerInput {
   @Field()
-  @IsNotEmpty()
   account: string;
 
   @Field()
-  @IsNotEmpty()
   password: string;
 
   @Field()
-  @IsNotEmpty()
   name: string;
 
   @Field()
-  @IsNotEmpty()
   phone: string;
 
   @Field()
-  @IsEmail()
-  @IsNotEmpty()
   email: string;
+
+  @Field()
+  avatar: string;
 }
