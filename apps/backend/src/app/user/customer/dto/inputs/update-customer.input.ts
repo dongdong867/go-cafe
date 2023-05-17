ff1aa7957b6747f423a1390aaf91f81dadc73893
dongdong867 <dongdong0867@gmail.com>
2023-05-18 03:53:05 +0800

    feat(update customer): remove unused  validator

--- apps/backend/src/app/user/customer/dto/inputs/update-customer.input.ts
+++ apps/backend/src/app/user/customer/dto/inputs/update-customer.input.ts
@@ -1,24 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsNotEmpty, IsOptional, IsEmail, IsUUID } from 'class-validator';
 
 @InputType()
 export class UpdateCustomerInput {
   @Field({ nullable: true })
-  @IsOptional()
   name?: string;
 
   @Field({ nullable: true })
-  @IsOptional()
   phone?: string;
 
   @Field({ nullable: true })
-  @IsEmail()
-  @IsOptional()
   email?: string;
-
-  // TODO: FEATURE WAITING
-  //
-  // @Field()
-  // @IsOptional()
-  // avatar: string
 }
