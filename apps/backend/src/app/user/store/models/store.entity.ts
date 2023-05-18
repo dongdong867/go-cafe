bfc6df0e7fe2e249f5edcc061ca5a1e8e9e9a96d
dongdong867 <dongdong0867@gmail.com>
2023-05-19 02:04:01 +0800

    feat(store entity): change fields naming

--- apps/backend/src/app/user/store/models/store.entity.ts
+++ apps/backend/src/app/user/store/models/store.entity.ts
@@ -1,12 +1,12 @@
 import { ObjectType, Field } from '@nestjs/graphql';
 import { User } from '../../models/user.entity';
 import { Rating } from '../../../rating/models/rating.entity';
-import { StoreRating } from './store-rating-entity';
+import { StoreRating } from './store-rating.entity';
 
 @ObjectType()
 export class Store {
   @Field(() => User)
-  User: User;
+  user: User;
 
   @Field()
   address: string;
@@ -15,5 +15,5 @@ export class Store {
   info: string;
 
   @Field(() => StoreRating)
-  StoreRating: StoreRating;
+  storeRating: StoreRating;
 }
