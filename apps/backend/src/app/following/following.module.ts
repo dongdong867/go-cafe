65365e4ea60bc8cc7247ed9413abb6b6d3d0dae0
dongdong867 <dongdong0867@gmail.com>
2023-05-19 00:20:53 +0800

    feat(following mo): add Prisma mo import

--- apps/backend/src/app/following/following.module.ts
+++ apps/backend/src/app/following/following.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FollowingService } from './following.service';
 import { FollowingResolver } from './following.resolver';
 import { UserModule } from '../user/user.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule],
   providers: [FollowingService, FollowingResolver],
 })
 export class FollowingModule {}
