3818f8e2e515dcb2d24bb6aa425a3035d0e71cc0
dongdong867 <dongdong0867@gmail.com>
2023-05-19 00:23:08 +0800

    feat(update store) remove validator and add avatar

--- apps/backend/src/app/user/store/dto/input/update-store.input.ts
+++ apps/backend/src/app/user/store/dto/input/update-store.input.ts
@@ -1,21 +1,19 @@
-import { IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
 import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateStoreInput {
   @Field({ nullable: true })
-  @IsOptional()
   name: string;
 
   @Field({ nullable: true })
-  @IsOptional()
   phone: string;
 
   @Field({ nullable: true })
-  @IsOptional()
   address: string;
 
   @Field({ nullable: true })
-  @IsOptional()
   info: string;
+
+  @Field({ nullable: true })
+  avatar: string;
 }
