9cdb374edcb655d28e43c6b5a7ca0c95245c00c1
dongdong867 <dongdong0867@gmail.com>
2023-05-18 23:44:41 +0800

    feat(get stores): remove validator

--- apps/backend/src/app/user/store/dto/args/get-stores.args.ts
+++ apps/backend/src/app/user/store/dto/args/get-stores.args.ts
@@ -1,9 +1,7 @@
 import { ArgsType, Field } from '@nestjs/graphql';
-import { IsNotEmpty } from 'class-validator';
 
 @ArgsType()
 export class GetStoresArgs {
   @Field(() => [String])
-  @IsNotEmpty()
   accounts: string[];
 }
