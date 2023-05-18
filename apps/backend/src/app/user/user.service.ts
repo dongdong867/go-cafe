fe6a32e9d011d99e9f4fc92aa85426c2747e9784
dongdong867 <dongdong0867@gmail.com>
2023-05-18 20:13:58 +0800

    feat(user s): add prisma fetch function in login

--- apps/backend/src/app/user/user.service.ts
+++ apps/backend/src/app/user/user.service.ts
@@ -1,9 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { Customer } from './customer/models/customer.entity';
-import { Store } from './store/models/store.entity';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { LoginInput } from './dto/input/login.input';
-import { v4 as uuidv4 } from 'uuid';
+import { PrismaService } from '../prisma/prisma.service';
+import { ForbiddenError } from '@nestjs/apollo';
 
 type PayloadType = {
   id: string;
@@ -14,16 +13,48 @@ type PayloadType = {
 
 @Injectable()
 export class UserService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService
+  ) {}
 
-  login(loginInput: LoginInput) {
+  async login(loginInput: LoginInput) {
     let role = '';
-    if (loginInput.account === 'test customer') role = 'customer';
-    else if (loginInput.account === 'test store') role = 'store';
-    else throw new BadRequestException('Login Failed');
+    let data = await this.prisma.customer.findFirst({
+      where: {
+        User: {
+          account: loginInput.account,
+          password: loginInput.password,
+        },
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (data === null) {
+      data = await this.prisma.store
+        .findFirstOrThrow({
+          where: {
+            User: {
+              account: loginInput.account,
+              password: loginInput.password,
+            },
+          },
+          select: {
+            id: true,
+          },
+        })
+        .catch(() => {
+          throw new ForbiddenError('login failed');
+        });
+      role = 'store';
+    } else {
+      role = 'customer';
+    }
 
     const token = this.jwtService.sign({
-      id: uuidv4(),
+      id: data.id,
       role: role,
     });
 
@@ -33,42 +64,20 @@ export class UserService {
   validateToken(token: string): {
     isValidate: boolean;
     id: string;
-    user: Customer | Store;
-    role: string;
+    role: 'customer' | 'store';
   } {
     const decodedToken = this.jwtService.decode(token) as PayloadType;
 
     if (decodedToken.role === 'customer') {
-      // TODO: REPLACE FAKE DATA WITH DB DATA
-      //
-      const user: Customer = {
-        account: 'test customer account',
-        name: 'test customer',
-        phone: '0912345678',
-        postCount: 0,
-        followCount: 0,
-        email: 'customer@example.com',
-        following: [],
-      };
       return {
         isValidate: true,
         id: decodedToken.id,
-        user: user,
         role: 'customer',
       };
     } else if (decodedToken.role === 'store') {
-      const user: Store = {
-        account: 'test store account',
-        address: 'store address',
-        info: 'store info',
-        name: 'test store',
-        phone: '0912345678',
-        postCount: 0,
-      };
       return {
         isValidate: true,
         id: decodedToken.id,
-        user: user,
         role: 'store',
       };
     }
