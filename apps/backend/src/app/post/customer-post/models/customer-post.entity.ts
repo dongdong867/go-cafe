c6b7a6968b39444f479323142ab5106902103647
dongdong867 <dongdong0867@gmail.com>
2023-05-20 04:08:45 +0800

    feat(customer entity): update entity fields

--- apps/backend/src/app/post/customer-post/models/customer-post.entity.ts
+++ apps/backend/src/app/post/customer-post/models/customer-post.entity.ts
@@ -1,12 +1,16 @@
 import { ObjectType, Field } from '@nestjs/graphql';
 import { Post } from '../../models/post.entity';
 import { Rating } from '../../../rating/models/rating.entity';
+import { Store } from '../../../user/store/models/store.entity';
 
 @ObjectType()
-export class UserPost extends Post {
-  @Field()
-  userAccount: string;
+export class CustomerPost {
+  @Field(() => Post)
+  post: Post;
 
   @Field(() => Rating)
   rating: Rating;
+
+  @Field(() => Store)
+  store: Store;
 }
