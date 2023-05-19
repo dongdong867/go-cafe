97a537bf7a322cc88850ca2871b5f74d19c9d3de
dongdong867 <dongdong0867@gmail.com>
2023-05-20 04:09:56 +0800

    refactor(post mo): update import module names

--- apps/backend/src/app/post/post.module.ts
+++ apps/backend/src/app/post/post.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StorePostModule } from './store-post/store-post.module';
-import { UserPostModule } from './user-post/user-post.module';
 import { UserModule } from '../user/user.module';
+import { CustomerPostModule } from './customer-post/customer-post.module';
 
 @Module({
-  imports: [StorePostModule, UserPostModule, UserModule],
+  imports: [StorePostModule, CustomerPostModule, UserModule],
 })
 export class PostModule {}
