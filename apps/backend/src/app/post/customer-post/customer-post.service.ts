0e310d4aa87f7b1d4d8148ac8b856047e2aa7997
dongdong867 <dongdong0867@gmail.com>
2023-05-20 04:05:07 +0800

    feat(customer-post s): add prisma to getPosts

--- apps/backend/src/app/post/customer-post/customer-post.service.ts
+++ apps/backend/src/app/post/customer-post/customer-post.service.ts
@@ -1,132 +1,155 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserPostInput } from './dto/input/create-user-post.input';
-import { UserPost } from './models/user-post.entity';
-import { Customer } from '../../user/customer/models/customer.entity';
-import { v4 as uuidv4 } from 'uuid';
-import { UpdateUserPostInput } from './dto/input/update-user-post.input';
-import { DeleteUserPostInput } from './dto/input/delete-user-post.input';
-import { RatingService } from '../../rating/rating.service';
-import { StoreService } from '../../user/store/store.service';
+import { CustomerPost } from './models/customer-post.entity';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class UserPostService {
+export class CustomerPostService {
   constructor(
-    private readonly ratingService: RatingService,
-    private readonly storeService: StoreService
+    // private readonly ratingService: RatingService,
+    // private readonly storeService: StoreService
+    private readonly prisma: PrismaService
   ) {}
 
-  getPosts(userId: string): UserPost[] {
-    return [
-      {
-        id: 'postId1',
-        userAccount: 'user 1',
-        storeAccount: 'store 1',
-        body: 'this is a fake body',
-        rating: {
-          general: 4,
-          environment: 3,
-          meals: 3,
-          attitude: 2,
+  async getPosts(currentId: string): Promise<CustomerPost[]> {
+    const data = await this.prisma.customer.findUniqueOrThrow({
+      where: {
+        id: currentId,
+      },
+      select: {
+        customerPost: {
+          select: {
+            post: {
+              select: {
+                body: true,
+                postPicture: {
+                  select: {
+                    picture: {
+                      select: {
+                        data: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            rating: {
+              select: {
+                general: true,
+                environment: true,
+                meals: true,
+                attitude: true,
+              },
+            },
+            store: {
+              select: {
+                user: {
+                  select: {
+                    account: true,
+                    name: true,
+                    phone: true,
+                    postCount: true,
+                    avatar: {
+                      select: {
+                        data: true,
+                      },
+                    },
+                  },
+                },
+                storeRating: {
+                  select: {
+                    postCount: true,
+                    rating: {
+                      select: {
+                        general: true,
+                        environment: true,
+                        meals: true,
+                        attitude: true,
+                      },
+                    },
+                  },
+                },
+                address: true,
+                info: true,
+              },
+            },
+          },
         },
       },
-      {
-        id: 'postId2',
-        userAccount: 'user 2',
-        storeAccount: 'store 2',
-        body: 'this is a fake body',
-        rating: {
-          general: 5,
-          environment: 2,
-          meals: 1,
-          attitude: 3,
-        },
-      },
-      {
-        id: 'postId3',
-        userAccount: 'user 3',
-        storeAccount: 'store 3',
-        body: 'this is a fake body',
-        rating: {
-          general: 2,
-          environment: 3,
-          meals: 4,
-          attitude: 4,
-        },
-      },
-    ];
+    });
+    return data.customerPost;
   }
 
-  createUserPost(
-    currentUser: Customer,
-    createUserPostInput: CreateUserPostInput
-  ): UserPost {
-    const post: UserPost = {
-      id: uuidv4(),
-      userAccount: currentUser.account,
-      ...createUserPostInput,
-    };
+  // createUserPost(
+  //   currentUser: Customer,
+  //   createUserPostInput: CreateUserPostInput
+  // ): UserPost {
+  //   const post: UserPost = {
+  //     id: uuidv4(),
+  //     userAccount: ,
+  //     ...createUserPostInput,
+  //   };
 
-    this.ratingService.addRating(
-      this.storeService.getStoreIdByAccount(createUserPostInput.storeAccount),
-      createUserPostInput.rating
-    );
+  //   this.ratingService.addRating(
+  //     this.storeService.getStoreIdByAccount(createUserPostInput.storeAccount),
+  //     createUserPostInput.rating
+  //   );
 
-    return post;
-  }
+  //   return post;
+  // }
 
-  updateUserPost(
-    currentUser: Customer,
-    updateUserPostInput: UpdateUserPostInput
-  ): UserPost {
-    const originPost: UserPost = {
-      id: 'postId1',
-      userAccount: 'user 1',
-      storeAccount: 'store 1',
-      body: 'this is a fake body',
-      rating: {
-        general: 4,
-        environment: 3,
-        meals: 3,
-        attitude: 2,
-      },
-    };
-    const newPost: UserPost = {
-      userAccount: currentUser.account,
-      storeAccount: 'test update store account',
-      ...updateUserPostInput,
-    };
+  // updateUserPost(
+  //   currentUser: Customer,
+  //   updateUserPostInput: UpdateUserPostInput
+  // ): UserPost {
+  //   const originPost: UserPost = {
+  //     id: 'postId1',
+  //     userAccount: 'user 1',
+  //     storeAccount: 'store 1',
+  //     body: 'this is a fake body',
+  //     rating: {
+  //       general: 4,
+  //       environment: 3,
+  //       meals: 3,
+  //       attitude: 2,
+  //     },
+  //   };
+  //   const newPost: UserPost = {
+  //     userAccount: currentUser.account,
+  //     storeAccount: 'test update store account',
+  //     ...updateUserPostInput,
+  //   };
 
-    const storeId = this.storeService.getStoreIdByAccount(
-      updateUserPostInput.storeAccount
-    );
-    this.ratingService.deleteRating(storeId, originPost.rating);
-    this.ratingService.addRating(storeId, updateUserPostInput.rating);
+  //   const storeId = this.storeService.getStoreIdByAccount(
+  //     updateUserPostInput.storeAccount
+  //   );
+  //   this.ratingService.deleteRating(storeId, originPost.rating);
+  //   this.ratingService.addRating(storeId, updateUserPostInput.rating);
 
-    return newPost;
-  }
+  //   return newPost;
+  // }
 
-  deleteUserPost(
-    currentUser: Customer,
-    deleteUserPostInput: DeleteUserPostInput
-  ): string {
-    const originPost: UserPost = {
-      id: 'postId1',
-      userAccount: 'user 1',
-      storeAccount: 'store 1',
-      body: 'this is a fake body',
-      rating: {
-        general: 4,
-        environment: 3,
-        meals: 3,
-        attitude: 2,
-      },
-    };
+  // async deleteUserPost(
+  //   currentUser: Customer,
+  //   deleteUserPostInput: DeleteUserPostInput
+  // ): Promise<string> {
+  //   const originPost: CustomerPost = {
+  //     id: 'postId1',
+  //     userAccount: 'user 1',
+  //     storeAccount: 'store 1',
+  //     body: 'this is a fake body',
+  //     rating: {
+  //       general: 4,
+  //       environment: 3,
+  //       meals: 3,
+  //       attitude: 2,
+  //     },
+  //   };
 
-    this.ratingService.deleteRating(
-      this.storeService.getStoreIdByAccount(originPost.storeAccount),
-      originPost.rating
-    );
+  //   this.ratingService.deleteRating(
+  //     await this.storeService.getStoreIdByAccount(originPost.storeAccount),
+  //     originPost.rating
+  //   );
 
-    return `delete post id: ${deleteUserPostInput.postId} successful`;
-  }
+  //   return `delete post id: ${deleteUserPostInput.postId} successful`;
+  // }
 }
