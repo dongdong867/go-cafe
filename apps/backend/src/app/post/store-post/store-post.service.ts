0974515f5bf83efd858807d0201e80ce74c336b8
dongdong867 <dongdong0867@gmail.com>
2023-05-20 19:13:34 +0800

    feat(store-post s): add prisma to getPosts method

--- apps/backend/src/app/post/store-post/store-post.service.ts
+++ apps/backend/src/app/post/store-post/store-post.service.ts
@@ -1,56 +1,79 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStorePostInput } from './dto/input/create-store-post.input';
-import { UpdateStorePostInput } from './dto/input/update-store-post.input';
 import { GetStorePostArgs } from './dto/args/get-store-post.args';
-import { StorePost } from './models/store-post.entity';
-import { v4 as uuidv4 } from 'uuid';
 import { Store } from '../../user/store/models/store.entity';
 import { DeleteStorePostInput } from './dto/input/delete-store-post.input';
+import { PrismaService } from '../../prisma/prisma.service';
+import { StorePost } from './models/store-post.entity';
+import { StoreService } from '../../user/store/store.service';
 
 @Injectable()
 export class StorePostService {
-  getPosts(getStorePostArgs: GetStorePostArgs): StorePost[] {
-    const posts: StorePost[] = [
-      {
-        id: uuidv4(),
-        title: 'test title',
-        storeAccount: getStorePostArgs.storeAccount,
-        body: 'test body',
+  constructor(
+    private readonly storeService: StoreService,
+    private readonly prisma: PrismaService
+  ) {}
+  async getPosts(getStorePostArgs: GetStorePostArgs): Promise<StorePost[]> {
+    const data = await this.prisma.store.findUniqueOrThrow({
+      where: {
+        id: await this.storeService.getStoreIdByAccount(
+          getStorePostArgs.storeAccount
+        ),
       },
-    ];
+      select: {
+        storePost: {
+          select: {
+            title: true,
+            post: {
+              select: {
+                body: true,
+                postPicture: {
+                  select: {
+                    picture: {
+                      select: {
+                        data: true,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
 
-    return posts;
+    return data.storePost;
   }
 
-  createStorePost(
-    currentUser: Store,
-    createStorePostInput: CreateStorePostInput
-  ): StorePost {
-    const post: StorePost = {
-      id: uuidv4(),
-      storeAccount: currentUser.account,
-      ...createStorePostInput,
-    };
+  // createStorePost(
+  //   currentUser: Store,
+  //   createStorePostInput: CreateStorePostInput
+  // ): StorePost {
+  //   const post: StorePost = {
+  //     id: uuidv4(),
+  //     storeAccount: currentUser.account,
+  //     ...createStorePostInput,
+  //   };
 
-    return post;
-  }
+  //   return post;
+  // }
 
-  updateStorePost(
-    currentUser: Store,
-    updateStorePostInput: UpdateStorePostInput
-  ): StorePost {
-    const post: StorePost = {
-      storeAccount: currentUser.account,
-      ...updateStorePostInput,
-    };
+  // updateStorePost(
+  //   currentUser: Store,
+  //   updateStorePostInput: UpdateStorePostInput
+  // ): StorePost {
+  //   const post: StorePost = {
+  //     storeAccount: currentUser.account,
+  //     ...updateStorePostInput,
+  //   };
 
-    return post;
-  }
+  //   return post;
+  // }
 
-  deleteStorePost(
-    currentUser: Store,
-    deleteStorePostInput: DeleteStorePostInput
-  ): string {
-    return `delete store post ${deleteStorePostInput.id} successfully`;
-  }
+  // deleteStorePost(
+  //   currentUser: Store,
+  //   deleteStorePostInput: DeleteStorePostInput
+  // ): string {
+  //   return `delete store post ${deleteStorePostInput.id} successfully`;
+  // }
 }
