f7b4f358e968569f1cbbc1896db280b491711077
dongdong867 <dongdong0867@gmail.com>
2023-05-20 18:56:15 +0800

    feat(store-post mo): setup Prisma mo import

--- apps/backend/src/app/post/store-post/store-post.module.ts
+++ apps/backend/src/app/post/store-post/store-post.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { StorePostService } from './store-post.service';
 import { StorePostResolver } from './store-post.resolver';
 import { UserModule } from '../../user/user.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule],
   providers: [StorePostResolver, StorePostService],
 })
 export class StorePostModule {}
