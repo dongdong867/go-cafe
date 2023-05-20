8f4910d88718d7ec81816c4988e22e2c89bd0853
dongdong867 <dongdong0867@gmail.com>
2023-05-20 14:48:46 +0800

    feat(customer-post): add id to customer post field

--- apps/backend/src/app/post/customer-post/models/customer-post.entity.ts
+++ apps/backend/src/app/post/customer-post/models/customer-post.entity.ts
@@ -1,10 +1,13 @@
-import { ObjectType, Field } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { Post } from '../../models/post.entity';
 import { Rating } from '../../../rating/models/rating.entity';
 import { Store } from '../../../user/store/models/store.entity';
 
 @ObjectType()
 export class CustomerPost {
+  @Field(() => ID)
+  id: string;
+
   @Field(() => Post)
   post: Post;
 
