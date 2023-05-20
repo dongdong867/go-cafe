271af87056d062feb5b0ad4da3f24927b74d5717
dongdong867 <dongdong0867@gmail.com>
2023-05-21 01:28:36 +0800

    feat(store-post): add async and change return type

--- apps/backend/src/app/post/store-post/store-post.resolver.ts
+++ apps/backend/src/app/post/store-post/store-post.resolver.ts
@@ -1,15 +1,14 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { StorePostService } from './store-post.service';
-import { CreateStorePostInput } from './dto/input/create-store-post.input';
-import { UpdateStorePostInput } from './dto/input/update-store-post.input';
 import { UseGuards } from '@nestjs/common';
 import { UserAuthGuard } from '../../user/guards/user-auth.guard';
 import { GetStorePostArgs } from './dto/args/get-store-post.args';
-import { CurrentUser } from '../../user/decorator/current-user.decorator';
-import { Store } from '../../user/store/models/store.entity';
 import { StorePost } from './models/store-post.entity';
-import { DeleteStorePostInput } from './dto/input/delete-store-post.input';
 import { StoreGuard } from '../../user/guards/role.guard';
+import { CurrentId } from '../../user/decorator/current-id.decorator';
+import { CreateStorePostInput } from './dto/input/create-store-post.input';
+import { UpdateStorePostInput } from './dto/input/update-store-post.input';
+import { DeleteStorePostInput } from './dto/input/delete-store-post.input';
 
 @UseGuards(UserAuthGuard)
 @Resolver(() => StorePost)
@@ -17,42 +16,44 @@ export class StorePostResolver {
   constructor(private readonly storePostService: StorePostService) {}
 
   @Query(() => [StorePost], { name: 'storePost', nullable: 'items' })
-  getPosts(@Args() getStorePostArgs: GetStorePostArgs): StorePost[] {
-    return this.storePostService.getPosts(getStorePostArgs);
+  async getPosts(
+    @Args() getStorePostArgs: GetStorePostArgs
+  ): Promise<StorePost[]> {
+    return await this.storePostService.getPosts(getStorePostArgs);
   }
 
   @UseGuards(StoreGuard)
-  @Mutation(() => StorePost)
-  createStorePost(
-    @CurrentUser() currentUser: Store,
+  @Mutation(() => String)
+  async createStorePost(
+    @CurrentId() currentId: string,
     @Args('createStorePostInput') createStorePostInput: CreateStorePostInput
-  ): StorePost {
-    return this.storePostService.createStorePost(
-      currentUser,
+  ): Promise<string> {
+    return await this.storePostService.createStorePost(
+      currentId,
       createStorePostInput
     );
   }
 
   @UseGuards(StoreGuard)
-  @Mutation(() => StorePost)
-  updateStorePost(
-    @CurrentUser() currentUser: Store,
+  @Mutation(() => String)
+  async updateStorePost(
+    @CurrentId() currentId: string,
     @Args('updateStorePostInput') updateStorePostInput: UpdateStorePostInput
-  ): StorePost {
+  ): Promise<string> {
     return this.storePostService.updateStorePost(
-      currentUser,
+      currentId,
       updateStorePostInput
     );
   }
 
   @UseGuards(StoreGuard)
   @Mutation(() => String)
-  deleteStorePost(
-    @CurrentUser() currentUser: Store,
+  async deleteStorePost(
+    @CurrentId() currentId: string,
     @Args('deleteStorePostInput') deleteStorePostInput: DeleteStorePostInput
-  ): string {
+  ): Promise<string> {
     return this.storePostService.deleteStorePost(
-      currentUser,
+      currentId,
       deleteStorePostInput
     );
   }
