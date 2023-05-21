432d708ab6b9ea31f70ff042d99285da3822fbd7
dongdong867 <dongdong0867@gmail.com>
2023-05-21 17:58:14 +0800

    feat(menu s): add firebase to createMenu method

--- apps/backend/src/app/menu/menu.service.ts
+++ apps/backend/src/app/menu/menu.service.ts
@@ -1,53 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { MenuInput } from './dto/input/menu.input';
-import { Menu } from './models/menu.entity';
-import { GetMenuArgs } from './dto/args/get-menu.args';
+import { FirebaseService } from '../firebase/firebase.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { MenuType } from './models/menu.firebase';
+import { v4 as uuid } from 'uuid';
+import * as admin from 'firebase-admin';
 
 @Injectable()
 export class MenuService {
-  getMenu(getMenuArgs: GetMenuArgs): Menu {
-    const fakeMenu: Menu = {
-      categories: [
-        {
-          name: 'coffee',
-          dishes: [
-            { name: 'america', price: 80 },
-            { name: 'latte', price: 120 },
-            { name: 'cappuccino', price: 130 },
-          ],
-        },
-        {
-          name: 'tea',
-          dishes: [
-            { name: 'black tea', price: 60 },
-            { name: 'green tea', price: 60 },
-            { name: 'milk tea', price: 100 },
-          ],
-        },
-        {
-          name: 'toast',
-          dishes: [
-            { name: 'apple toast', price: 200 },
-            { name: 'banana toast', price: 200 },
-            { name: 'guava toast', price: 200 },
-          ],
-        },
-      ],
+  constructor(
+    private readonly firebase: FirebaseService,
+    private readonly prisma: PrismaService
+  ) {}
+
+  // async getMenu(getMenuArgs: GetMenuArgs): Promise<Menu> {
+  //   return null
+  // }
+
+  async createMenu(
+    currentId: string,
+    createMenuInput: MenuInput
+  ): Promise<string> {
+    await this.prisma.store.findUniqueOrThrow({
+      where: {
+        id: currentId,
+      },
+    });
+
+    const menu: MenuType = {
+      id: uuid(),
+      store_id: currentId,
+      menu: createMenuInput.categories.map((category) => ({
+        category_name: category.name,
+        dishes: category.dishes.map((dish) => ({
+          dish_name: dish.name,
+          price: dish.price,
+        })),
+      })),
+      update_at: admin.firestore.FieldValue.serverTimestamp(),
     };
-    return fakeMenu;
+
+    await this.firebase.firestore().collection('menu').doc(menu.id).set(menu);
+
+    return 'menu create successfully';
   }
 
-  createMenu(storeId: string, createMenuInput: MenuInput): Menu {
-    const menu: Menu = {
-      categories: createMenuInput.categories,
-    };
-    return menu;
-  }
-
-  updateMenu(storeId: string, updateMenuInput: MenuInput): Menu {
-    const menu: Menu = {
-      categories: updateMenuInput.categories,
-    };
-    return menu;
-  }
+  // updateMenu(storeId: string, updateMenuInput: MenuInput): Menu {
+  //   const menu: Menu = {
+  //     categories: updateMenuInput.categories,
+  //   };
+  //   return menu;
+  // }
 }
