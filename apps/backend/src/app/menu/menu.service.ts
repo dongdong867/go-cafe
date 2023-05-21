90ab143767dcf27177bcaa80cd808694a77a9902
dongdong867 <dongdong0867@gmail.com>
2023-05-21 22:26:15 +0800

    feat(menu s): add firebase to getMenu method

--- apps/backend/src/app/menu/menu.service.ts
+++ apps/backend/src/app/menu/menu.service.ts
@@ -1,22 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { FirebaseService } from '../firebase/firebase.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { MenuType } from './models/menu.firebase';
+import { CategoryType, DishType, MenuType } from './models/menu.firebase';
 import { v4 as uuid } from 'uuid';
 import * as admin from 'firebase-admin';
 import { CreateMenuInput } from './dto/input/create-menu.input';
 import { UpdateMenuInput } from './dto/input/update-menu.input';
+import { GetMenuArgs } from './dto/args/get-menu.args';
+import { Category, Dish, Menu } from './models/menu.entity';
+import { StoreService } from '../user/store/store.service';
 
 @Injectable()
 export class MenuService {
   constructor(
+    private readonly storeService: StoreService,
     private readonly firebase: FirebaseService,
     private readonly prisma: PrismaService
   ) {}
 
-  // async getMenu(getMenuArgs: GetMenuArgs): Promise<Menu> {
-  //   return null
-  // }
+  async getMenu(getMenuArgs: GetMenuArgs): Promise<Menu> {
+    const storeId: string = await this.storeService.getStoreIdByAccount(
+      getMenuArgs.storeAccount
+    );
+    const data = await this.firebase
+      .firestore()
+      .collection('menu')
+      .where('store_id', '==', storeId)
+      .get();
+
+    const menu: Menu[] = data.docs.map(
+      (menu): Menu => ({
+        categories: menu.get('menu').map(
+          (category: CategoryType): Category => ({
+            name: category.category_name,
+            dishes: category.dishes.map(
+              (dish: DishType): Dish => ({
+                name: dish.dish_name,
+                price: dish.price,
+              })
+            ),
+          })
+        ),
+      })
+    );
+
+    return menu[0];
+  }
 
   async createMenu(
     currentId: string,
