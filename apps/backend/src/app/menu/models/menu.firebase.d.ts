8c18cff2a093d0b15845512f7a9d49fc97bcb70f
dongdong867 <dongdong0867@gmail.com>
2023-05-21 22:25:28 +0800

    refactor(menu type): add category and dish type

--- apps/backend/src/app/menu/models/menu.firebase.d.ts
+++ apps/backend/src/app/menu/models/menu.firebase.d.ts
@@ -3,12 +3,16 @@ import * as admin from 'firebase-admin';
 type MenuType = {
   id: string;
   store_id: string;
-  menu: {
-    category_name: string;
-    dishes: {
-      dish_name: string;
-      price: number;
-    }[];
-  }[];
+  menu: CategoryType[];
   update_at: admin.firestore.FieldValue;
+};
+
+type CategoryType = {
+  category_name: string;
+  dishes: DishType[];
+};
+
+type DishType = {
+  dish_name: string;
+  price: number;
 };
