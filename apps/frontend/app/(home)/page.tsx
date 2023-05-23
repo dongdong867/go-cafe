430a4d6c8c7831b5804170895d1cea0bdf51c9b2
dongdong867 <dongdong0867@gmail.com>
2023-05-23 16:32:00 +0800

    feat(home): add graphql query

--- apps/frontend/app/(home)/page.tsx
+++ apps/frontend/app/(home)/page.tsx
@@ -1,15 +1,54 @@
-import userPostList from '@/../public/data/UserPostList';
+import { getClient } from '@/../lib/client';
 import SearchBar from '@/components/Input/SearchBar';
 import UserPostModal from '@/components/UserPostModal';
+import { gql } from '@apollo/client';
 
-const Home = () => {
+const query = gql`
+  query CustomerPost {
+    customerPost {
+      id
+      post {
+        body
+        postPicture {
+          picture {
+            data
+          }
+        }
+      }
+      rating {
+        general
+        environment
+        meals
+        attitude
+      }
+      store {
+        user {
+          account
+          name
+        }
+      }
+      customer {
+        user {
+          account
+          name
+        }
+      }
+    }
+  }
+`;
+
+const Home = async () => {
+  const client = getClient();
+  const { data } = await client.query({ query });
+  console.log(data.customerPost);
+
   return (
     <>
       <div className="w-full h-full flex flex-col justify-start place-items-center">
         <SearchBar route="home" />
         <div className="pb-4">
-          {userPostList.map((post) => (
-            <UserPostModal key={post.id} post={post} />
+          {data.customerPost.map((customerPost) => (
+            <UserPostModal key={customerPost.id} customerPost={customerPost} />
           ))}
         </div>
       </div>
