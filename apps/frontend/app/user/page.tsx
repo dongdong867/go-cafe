5c26a3d9cf6fb7db5a2e29b44708d97fb724f3b7
dongdong867 <dongdong0867@gmail.com>
2023-05-24 16:32:46 +0800

    feat(UserPage): implement ApolloClient

--- apps/frontend/app/user/page.tsx
+++ apps/frontend/app/user/page.tsx
@@ -1,20 +1,68 @@
-import UserPostList from '@/../public/data/UserPostList';
-
+import { gql } from '@apollo/client';
 import UserInfo from './components/UserInfo';
 import UserPostModal from '@/components/UserPostModal';
+import { getClient } from '@/../lib/client';
 
-const UserPage = () => {
+const query = gql`
+  query Customer {
+    customer {
+      user {
+        avatar {
+          data
+        }
+        account
+        name
+        phone
+        postCount
+      }
+      email
+      followingCount
+    }
+    customerPost {
+      id
+      post {
+        body
+        postPicture {
+          picture {
+            data
+          }
+        }
+      }
+      rating {
+        general
+        environment
+        meals
+        attitude
+      }
+      store {
+        user {
+          account
+          name
+        }
+      }
+      customer {
+        user {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const UserPage = async () => {
+  const client = getClient();
+  const { data } = await client.query({ query });
   return (
     <>
       <div className="w-full h-fit max-w-lg max-[450px]:w-11/12 m-auto">
-        <UserInfo />
+        <UserInfo data={data.customer} />
 
         <div className="divider" />
 
         <div>
           <div className="text-2xl font-semibold my-4">History</div>
-          {UserPostList.map((post) => (
-            <UserPostModal key={post.id} editable post={post} />
+          {data.customerPost.map((post) => (
+            <UserPostModal key={post.id} editable customerPost={post} />
           ))}
         </div>
       </div>
