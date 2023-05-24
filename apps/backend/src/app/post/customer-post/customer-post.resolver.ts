a7cafea6e409b25bfacd97266828454c121e30c8
dongdong867 <dongdong0867@gmail.com>
2023-05-25 00:09:07 +0800

    feat(customer post r): add query by store account

--- apps/backend/src/app/post/customer-post/customer-post.resolver.ts
+++ apps/backend/src/app/post/customer-post/customer-post.resolver.ts
@@ -9,6 +9,7 @@ import { CreateCustomerPostInput } from './dto/input/create-customer-post.input'
 import { UpdateCustomerPostInput } from './dto/input/update-customer-post.input';
 import { DeleteCustomerPostInput } from './dto/input/delete-customer-post.input';
 import { GetCustomerPostArgs } from './dto/args/get-customer-post.args';
+import { GetCustomerPostAtStoreArgs } from './dto/args/get-customer-post-at-store.args';
 
 @UseGuards(UserAuthGuard, CustomerGuard)
 @Resolver(() => CustomerPost)
@@ -34,6 +35,20 @@ export class CustomerPostResolver {
   @Query(() => [CustomerPost], { name: 'selfPost', nullable: 'items' })
   async getSelfPosts(@CurrentId() currentId: string): Promise<CustomerPost[]> {
     return await this.customerPostService.getSelfPosts(currentId);
+  }
+
+  @Query(() => [CustomerPost], {
+    name: 'customerPostAtStore',
+    nullable: 'items',
+  })
+  async getPostByStoreAccount(
+    @CurrentId() currentId: string,
+    @Args() getCustomerPostAtStoreArgs: GetCustomerPostAtStoreArgs
+  ) {
+    return await this.customerPostService.getPostsByStoreAccount(
+      currentId,
+      getCustomerPostAtStoreArgs
+    );
   }
 
   @Mutation(() => String)
