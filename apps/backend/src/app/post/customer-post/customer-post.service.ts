e17f4c2c9890c32a16cb381ce741bb5f50179e61
dongdong867 <dongdong0867@gmail.com>
2023-05-24 18:46:59 +0800

    feat(customer post s): add getSelfPosts method

--- apps/backend/src/app/post/customer-post/customer-post.service.ts
+++ apps/backend/src/app/post/customer-post/customer-post.service.ts
@@ -95,6 +95,83 @@ export class CustomerPostService {
     return data;
   }
 
+  async getSelfPosts(currentId: string): Promise<CustomerPost[]> {
+    return await this.prisma.customerPost.findMany({
+      where: {
+        customerId: currentId,
+      },
+      select: {
+        id: true,
+        rating: true,
+        store: {
+          select: {
+            user: {
+              select: {
+                account: true,
+                name: true,
+                phone: true,
+                postCount: true,
+                avatar: {
+                  select: {
+                    data: true,
+                  },
+                },
+              },
+            },
+            storeRating: {
+              select: {
+                postCount: true,
+                rating: {
+                  select: {
+                    general: true,
+                    environment: true,
+                    meals: true,
+                    attitude: true,
+                  },
+                },
+              },
+            },
+            address: true,
+            info: true,
+          },
+        },
+        customer: {
+          select: {
+            user: {
+              select: {
+                account: true,
+                name: true,
+                phone: true,
+                postCount: true,
+                avatar: {
+                  select: {
+                    data: true,
+                  },
+                },
+              },
+            },
+            followingCount: true,
+            email: true,
+          },
+        },
+        post: {
+          select: {
+            body: true,
+            postPicture: {
+              select: {
+                picture: {
+                  select: {
+                    data: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
   async createCustomerPost(
     currentId: string,
     createUserPostInput: CreateCustomerPostInput
