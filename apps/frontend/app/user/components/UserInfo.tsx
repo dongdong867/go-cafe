13d86fc4fe1a364868a97ae3a460abe5a5186b3b
dongdong867 <dongdong0867@gmail.com>
2023-05-24 16:31:48 +0800

    feat(UserInfo): update props name

--- apps/frontend/app/user/components/UserInfo.tsx
+++ apps/frontend/app/user/components/UserInfo.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 
 import TemporaryImage from '/public/images/logo.png';
-const UserInfo = () => {
+
+type Props = {
+  data: User;
+};
+
+const UserInfo = ({ data }: Props) => {
   return (
     <>
       <div className="w-full flex justify-around place-items-center pt-5">
@@ -17,17 +22,17 @@ const UserInfo = () => {
 
         <div className="space-y-2">
           <div className="text-4xl max-[450px]:text-3xl font-bold">
-            User Name
+            {data.user.name}
           </div>
           <div className="flex text-xl max-[450px]:text-lg font-medium">
             <div>
               <div>Posts</div>
-              <div>123</div>
+              <div>{data.user.postCount}</div>
             </div>
             <div className="divider divider-horizontal"></div>
             <div>
               <div>Following</div>
-              <div>12345</div>
+              <div>{data.followingCount}</div>
             </div>
           </div>
         </div>
