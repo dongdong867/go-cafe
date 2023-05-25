99187ea92c04bb09943e6e205b2db5527bba9508
dongdong867 <dongdong0867@gmail.com>
2023-05-25 19:38:51 +0800

    feat(home): change graphql query name

--- apps/frontend/app/(home)/page.tsx
+++ apps/frontend/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import { gql } from '@apollo/client';
 
 const query = gql`
   query CustomerPost {
-    customerPost {
+    customerPosts {
       id
       post {
         body
@@ -40,14 +40,13 @@ const query = gql`
 const Home = async () => {
   const client = getClient();
   const { data } = await client.query({ query });
-  console.log(data.customerPost);
 
   return (
     <>
       <div className="w-full h-full flex flex-col justify-start place-items-center">
         <SearchBar route="home" />
         <div className="pb-4">
-          {data.customerPost.map((customerPost) => (
+          {data.customerPosts.map((customerPost) => (
             <UserPostModal key={customerPost.id} customerPost={customerPost} />
           ))}
         </div>
