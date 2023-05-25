5ed6c1c298a8bf135790160284171b3998a1d589
dongdong867 <dongdong0867@gmail.com>
2023-05-26 06:08:17 +0800

    refactor(rating): delete unused resource

--- apps/backend/src/app/rating/rating.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RatingService } from './rating.service';
-
-@Module({
-  providers: [RatingService],
-  exports: [RatingService],
-})
-export class RatingModule {}
