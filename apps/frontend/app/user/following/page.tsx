6d1d954cf7cb4e01ee1ba906eb7b501b46a7388e
dongdong867 <dongdong0867@gmail.com>
2023-05-25 19:44:50 +0800

    feat(following): implement ApolloClient

--- apps/frontend/app/user/following/page.tsx
+++ apps/frontend/app/user/following/page.tsx
@@ -1,15 +1,44 @@
 import PageTitle from '@/components/PageTitle';
 import ShopCardModal from './components/ShopCardModal';
+import { gql } from '@apollo/client';
+import { getClient } from '@/../lib/client';
 
-import FollowingList from '@/../public/data/FollowingList';
+const query = gql`
+  query FollowingList {
+    followingList {
+      address
+      info
+      storeRating {
+        postCount
+        rating {
+          general
+          environment
+          meals
+          attitude
+        }
+      }
+      user {
+        account
+        avatar {
+          data
+        }
+        name
+        phone
+        postCount
+      }
+    }
+  }
+`;
 
-const FollowingPage = () => {
+const FollowingPage = async () => {
+  const client = getClient();
+  const { data } = await client.query({ query });
   return (
     <>
       <div className="w-full max-w-lg max-[450px]:w-11/12 m-auto">
         <PageTitle title="Following List" />
         <div className="flex flex-col space-y-4">
-          {FollowingList.map((shop) => (
+          {data.followingList.map((shop) => (
             <ShopCardModal shop={shop} />
           ))}
         </div>
