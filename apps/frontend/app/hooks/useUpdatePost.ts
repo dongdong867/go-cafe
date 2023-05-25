c5b0a713f89b5f5a70f5bfc98d0ed2490c2229c5
dongdong867 <dongdong0867@gmail.com>
2023-05-25 19:38:03 +0800

    feat(useUpdatePost): implement ApolloClient

--- apps/frontend/app/hooks/useUpdatePost.ts
+++ apps/frontend/app/hooks/useUpdatePost.ts
@@ -1,22 +1,97 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useRating from './useRating';
+import { gql, useMutation } from '@apollo/client';
+import { useSuspenseQuery } from '@apollo/experimental-nextjs-app-support/ssr';
+import useToast from './useToast';
+import { redirect } from 'next/navigation';
 
-const useUpdatePost = (originPost: UserPost) => {
-  const [body, setBody] = useState(originPost.body);
+const query = gql`
+  query CustomerPost($postId: ID!) {
+    customerPost(postId: $postId) {
+      id
+      post {
+        body
+        postPicture {
+          picture {
+            data
+          }
+        }
+      }
+      rating {
+        general
+        environment
+        meals
+        attitude
+      }
+      store {
+        user {
+          account
+          name
+        }
+      }
+      customer {
+        user {
+          name
+        }
+      }
+    }
+  }
+`;
 
-  const { rating, setRate } = useRating(originPost.rating);
+const UPDATE_POST = gql`
+  mutation UpdateCustomerPost(
+    $updateCustomerPostInput: UpdateCustomerPostInput!
+  ) {
+    updateCustomerPost(updateCustomerPostUInput: $updateCustomerPostInput)
+  }
+`;
 
-  const update = () => {
-    const post = {
-      coffeeShop: originPost.coffeeShop,
-      rating: rating,
-      body: body,
-    };
+const useUpdatePost = (postId: string) => {
+  const { data: originData }: { data: { customerPost: UserPost } } =
+    useSuspenseQuery(query, {
+      variables: {
+        postId: postId,
+      },
+    });
 
-    console.log(post);
+  const [body, setBody] = useState(originData.customerPost.post.body);
+  const { rating, setRate } = useRating(originData.customerPost.rating);
+
+  const [updateCustomerPost, { data, error }] = useMutation(UPDATE_POST);
+
+  useEffect(() => {
+    if (data) redirect('/');
+  }, [data]);
+
+  if (error) {
+    console.log(error);
+  }
+
+  const updatePost = async () => {
+    await updateCustomerPost({
+      variables: {
+        updateCustomerPostInput: {
+          id: originData.customerPost.id,
+          body: body,
+          rating: {
+            general: rating.general,
+            environment: rating.environment,
+            meals: rating.meals,
+            attitude: rating.attitude,
+          },
+        },
+      },
+    });
   };
 
-  return { rating, setRate, setBody, update };
+  return {
+    body,
+    rating,
+    shop: originData.customerPost.store,
+    setRate,
+    setBody,
+    updatePost,
+  };
 };
 
 export default useUpdatePost;
