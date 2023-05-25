fd97f4667fa45dd99a3ec2a5e57588f886fa3370
dongdong867 <dongdong0867@gmail.com>
2023-05-26 06:08:44 +0800

    feat(customer post mo): remove unused import

--- apps/backend/src/app/post/customer-post/customer-post.module.ts
+++ apps/backend/src/app/post/customer-post/customer-post.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../../user/user.module';
-import { RatingModule } from '../../rating/rating.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { CustomerPostService } from './customer-post.service';
 import { CustomerPostResolver } from './customer-post.resolver';
 
 @Module({
-  imports: [UserModule, RatingModule, PrismaModule],
+  imports: [UserModule, PrismaModule],
   providers: [CustomerPostResolver, CustomerPostService],
 })
 export class CustomerPostModule {}
