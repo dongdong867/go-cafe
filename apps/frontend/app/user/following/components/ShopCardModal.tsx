44ea93c183793846c7d09aa79f67e1e03aa6d184
dongdong867 <dongdong0867@gmail.com>
2023-05-25 19:45:50 +0800

    feat(ShopCard): change data type and structure

--- apps/frontend/app/user/following/components/ShopCardModal.tsx
+++ apps/frontend/app/user/following/components/ShopCardModal.tsx
@@ -4,13 +4,13 @@ import TemporaryPicture from '/public/images/logo.png';
 import Link from 'next/link';
 
 type Props = {
-  shop: ShopCardForFollowingList;
+  shop: Shop;
 };
 
 const ShopCardModal = ({ shop }: Props) => {
   return (
     <>
-      <Link href={`/shop/${shop.id}`}>
+      <Link href={`/shop/${shop.user.account}`}>
         <div className="card card-side bg-base-100 shadow-[0_4px_15px_rgba(0,0,0,0.25)]">
           <figure className="p-4">
             <div className="avatar w-28 h-28">
@@ -23,17 +23,17 @@ const ShopCardModal = ({ shop }: Props) => {
           </figure>
           <div className="card-body justify-around items-right text-right pl-0 pr-4 py-4">
             <div className="card-title text-2xl font-bold justify-end">
-              {shop.name}
+              {shop.user.name}
             </div>
-            <div className="text-lg max-[450px]:text-sm font-medium">
+            <div className="text-lg max-[450px]:text-sm font-semibold">
               <div className="flex justify-end space-x-1">
                 <div>Tel: </div>
-                <div className="font-semibold">{shop.telephone}</div>
+                <div>{shop.user.phone}</div>
               </div>
 
-              <div className="text-right min-[450px]:flex min-[450px]:justify-end">
-                <div>Opening Time: </div>
-                <div className="font-semibold">{shop.openingTime}</div>
+              <div className="flex justify-end space-x-1">
+                <div>Address: </div>
+                <div>{shop.address}</div>
               </div>
             </div>
           </div>
