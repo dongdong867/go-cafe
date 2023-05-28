760da9d8fa19ae5899ff5d1cb927d479dead2512
dongdong867 <dongdong0867@gmail.com>
2023-05-29 03:39:57 +0800

    feat(useRegister): implement register functions

--- apps/frontend/app/hooks/useRegister.ts
+++ apps/frontend/app/hooks/useRegister.ts
@@ -1,24 +1,130 @@
+import { uploadPicture } from '@/../lib/picture-upload';
+import { gql, useMutation } from '@apollo/client';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { toast } from 'react-hot-toast';
+
+const CREATE_CUSTOMER = gql`
+  mutation CreateCustomer($createCustomerInput: CreateCustomerInput!) {
+    createCustomer(createCustomerInput: $createCustomerInput)
+  }
+`;
+
+const CREATE_SHOP = gql`
+  mutation CreateStore($createStoreInput: CreateStoreInput!) {
+    createStore(createStoreInput: $createStoreInput)
+  }
+`;
 
 const useRegister = () => {
+  // basic account info
   const [account, setAccount] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+  const [avatar, setAvatar] = useState(null as File);
 
-  const register = () => {
-    const user = {
-      account: account,
-      password: password,
-      name: name,
-      email: email,
-      phone: phone,
+  // customer info
+  const [email, setEmail] = useState('');
+
+  // shop info
+  const [address, setAddress] = useState('');
+  const [info, setInfo] = useState('');
+
+  const router = useRouter();
+
+  // mutation
+  const [createCustomer] = useMutation(CREATE_CUSTOMER);
+  const [createStore] = useMutation(CREATE_SHOP);
+
+  // handle register
+  const registerCustomer = async () => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(avatar);
+    fileReader.onload = async () => {
+      const avatarUrl = await uploadPicture(fileReader.result as string);
+
+      const create = createCustomer({
+        variables: {
+          createCustomerInput: {
+            account: account,
+            password: password,
+            name: name,
+            phone: phone,
+            avatar: avatarUrl,
+            email: email,
+          },
+        },
+      }).then(() => router.push('/'));
+
+      toast.promise(
+        create,
+        {
+          loading: 'Creating...',
+          success: 'Account Created \n Please Login Again',
+          error: (error) => error.message,
+        },
+        {
+          className: 'font-bold text-lg',
+        }
+      );
     };
-    console.log(user);
   };
 
-  return { setAccount, setPassword, setName, setEmail, setPhone, register };
+  const registerShop = async () => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(avatar);
+    fileReader.onload = async () => {
+      const avatarUrl = await uploadPicture(fileReader.result as string);
+
+      const create = createStore({
+        variables: {
+          createStoreInput: {
+            account: account,
+            password: password,
+            name: name,
+            phone: phone,
+            avatar: avatarUrl,
+            address: address,
+            info: info,
+          },
+        },
+      }).then(() => router.push('/'));
+
+      toast.promise(
+        create,
+        {
+          loading: 'Creating...',
+          success: 'Account Created \n Please Login Again',
+          error: (error) => error.message,
+        },
+        {
+          className: 'font-bold text-lg',
+        }
+      );
+    };
+  };
+
+  return {
+    account,
+    password,
+    name,
+    phone,
+    avatar,
+    email,
+    address,
+    info,
+    setAccount,
+    setPassword,
+    setName,
+    setPhone,
+    setAvatar,
+    setEmail,
+    setAddress,
+    setInfo,
+    registerCustomer,
+    registerShop,
+  };
 };
 
 export default useRegister;
