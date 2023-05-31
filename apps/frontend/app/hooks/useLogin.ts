a5c7793b74b0e02e89d4acf0781e7bbef62d847e
dongdong867 <dongdong0867@gmail.com>
2023-05-31 23:38:21 +0800

    fix(useLogin): fix error handler error

--- apps/frontend/app/hooks/useLogin.ts
+++ apps/frontend/app/hooks/useLogin.ts
@@ -1,6 +1,6 @@
 import { gql, useMutation } from '@apollo/client';
-import { redirect, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import toast from 'react-hot-toast';
 
 const LOGIN_IN = gql`
@@ -32,35 +32,35 @@ const useLogin = () => {
           password: password,
         },
       },
-    })
-      .then(
-        (res: {
-          data: { login: { token: string; role: string; avatar: string } };
-        }) => {
-          document.cookie = `token=${res.data.login.token}`;
-          document.cookie = `role=${res.data.login.role}`;
-          document.cookie = `avatar=${res.data.login.avatar}`;
-          localStorage.setItem('token', res.data.login.token);
-          localStorage.setItem('role', res.data.login.role);
-          router.push('/');
-        }
-      )
-      .catch(() => reset());
-
-    toast.promise(
-      loginPromise,
-      {
-        loading: 'Loading...',
-        success: 'Login Success',
-        error: (error) => error.message,
-      },
-      {
-        className: 'font-bold text-lg',
+    }).then(
+      (res: {
+        data: { login: { token: string; role: string; avatar: string } };
+      }) => {
+        document.cookie = `token=${res.data.login.token}`;
+        document.cookie = `role=${res.data.login.role}`;
+        document.cookie = `avatar=${res.data.login.avatar}`;
+        localStorage.setItem('token', res.data.login.token);
+        localStorage.setItem('role', res.data.login.role);
+        router.push('/');
       }
     );
+
+    await toast
+      .promise(
+        loginPromise,
+        {
+          loading: 'Loading...',
+          success: 'Login Success',
+          error: (err) => err.message,
+        },
+        {
+          className: 'font-bold text-lg',
+        }
+      )
+      .catch((err) => reset());
   };
 
-  return { setAccount, setPassword, handleLogin };
+  return { account, setAccount, setPassword, handleLogin };
 };
 
 export default useLogin;
