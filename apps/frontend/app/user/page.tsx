067f4761d3b5497f9bb37799690e22485ef1782d
dongdong867 <dongdong0867@gmail.com>
2023-06-02 01:59:23 +0800

    feat(UserPage): add shop role to user page

--- apps/frontend/app/user/page.tsx
+++ apps/frontend/app/user/page.tsx
@@ -1,8 +1,10 @@
 import { gql } from '@apollo/client';
-import UserInfo from './components/UserInfo';
 import UserPostModal from '@/components/UserPostModal';
 import { getClient } from '@/../lib/client';
 import { cookies } from 'next/headers';
+import ShopPostModal from '@/shop/components/ShopPostModal';
+import CustomerProfile from './components/CustomerProfile';
+import UserShopProfile from './components/ShopProfile';
 
 const customerQuery = gql`
   query Customer {
@@ -75,27 +77,58 @@ const shopQuery = gql`
         }
       }
     }
+    storeSelfPost {
+      post {
+        body
+        postPicture {
+          picture {
+            data
+          }
+        }
+      }
+      id
+      title
+    }
   }
 `;
 
 const UserPage = async () => {
+  const role = cookies().get('role').value;
+
   const client = getClient();
   const { data } =
-    cookies().get('role').value === 'customer'
+    role === 'customer'
       ? await client.query({ query: customerQuery })
       : await client.query({ query: shopQuery });
+
+  const Info = () => {
+    if (role === 'customer') {
+      return <CustomerProfile data={data.customer} />;
+    } else {
+      return <UserShopProfile data={data.storeSelf} />;
+    }
+  };
+
+  const PostList = () => {
+    if (role === 'customer') {
+      return data.selfPost.map((post) => (
+        <UserPostModal key={post.id} editable customerPost={post} />
+      ));
+    } else {
+      return data.storeSelfPost.map((post) => (
+        <ShopPostModal key={post.id} editable post={post} />
+      ));
+    }
+  };
   return (
     <>
       <div className="w-full h-fit max-w-lg max-[450px]:w-11/12 m-auto">
-        <UserInfo data={data.customer} />
-
-        <div className="divider" />
+        <Info />
 
         <div>
-          <div className="text-2xl font-semibold my-4">History</div>
-          {data.selfPost.map((post) => (
-            <UserPostModal key={post.id} editable customerPost={post} />
-          ))}
+          <div className="text-2xl font-bold mt-8">History Post</div>
+          <div className="divider my-2" />
+          <PostList />
         </div>
       </div>
     </>
