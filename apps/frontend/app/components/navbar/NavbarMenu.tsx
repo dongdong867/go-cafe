17aa10ab9b3829edac328f3d6b853d1134e349db
dongdong867 <dongdong0867@gmail.com>
2023-06-04 06:04:25 +0800

    feat(NavbarMenu): add shop selections

--- apps/frontend/app/components/navbar/NavbarMenu.tsx
+++ apps/frontend/app/components/navbar/NavbarMenu.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 
 //icons
 import { CgMenuLeft } from 'react-icons/cg';
-import { FaBell, FaUserFriends } from 'react-icons/fa';
+import { FaConciergeBell, FaUserFriends } from 'react-icons/fa';
 import { MdOutlinePostAdd, MdInfo } from 'react-icons/md';
 import DropdownMenuModal from './DropdownMenuModal';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 const Menu = () => {
-  const createPostHref =
-    cookies().get('role').value === 'store'
-      ? '/post/create/shopPost'
-      : '/post/create';
+  if (!cookies().has('role')) redirect('/login');
+
+  const role = cookies().get('role').value;
 
   const button = (
     <>
@@ -22,29 +22,55 @@ const Menu = () => {
     </>
   );
 
-  const listItems = (
-    <>
-      <li className="menu-title px-4 pt-4">menu list</li>
-      <li>
-        <Link href={'/user/following'} className="p-4 text-xl font-semibold">
-          <FaUserFriends />
-          <div>Following list</div>
-        </Link>
-      </li>
-      <li>
-        <Link href={createPostHref} className="p-4 text-xl font-semibold">
-          <MdOutlinePostAdd />
-          <div>Create Post</div>
-        </Link>
-      </li>
-      <li>
-        <Link href={'/'} className="p-4 text-xl font-semibold">
-          <MdInfo />
-          <div>About Go Cafe</div>
-        </Link>
-      </li>
-    </>
-  );
+  const listItems =
+    role === 'customer' ? (
+      <>
+        <li className="menu-title px-4 pt-4">menu list</li>
+        <li>
+          <Link href={'/user/following'} className="p-4 text-xl font-semibold">
+            <FaUserFriends />
+            <div>Following list</div>
+          </Link>
+        </li>
+        <li>
+          <Link href={'/post/create'} className="p-4 text-xl font-semibold">
+            <MdOutlinePostAdd />
+            <div>Create Post</div>
+          </Link>
+        </li>
+        <li>
+          <Link href={'/'} className="p-4 text-xl font-semibold">
+            <MdInfo />
+            <div>About Go Cafe</div>
+          </Link>
+        </li>
+      </>
+    ) : (
+      <>
+        <li className="menu-title px-4 pt-4">menu list</li>
+        <li>
+          <Link
+            href={'/post/create/shopPost'}
+            className="p-4 text-xl font-semibold"
+          >
+            <MdOutlinePostAdd />
+            <div>Create Post</div>
+          </Link>
+        </li>
+        <li>
+          <Link href={'/order'} className="p-4 text-xl font-semibold">
+            <FaConciergeBell />
+            <div>Orders</div>
+          </Link>
+        </li>
+        <li>
+          <Link href={'/'} className="p-4 text-xl font-semibold">
+            <MdInfo />
+            <div>About Go Cafe</div>
+          </Link>
+        </li>
+      </>
+    );
 
   return (
     <>
