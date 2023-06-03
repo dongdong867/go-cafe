33909abc5f2e3ce6d410c81dff0f8556da5e6927
dongdong867 <dongdong0867@gmail.com>
2023-06-04 06:06:10 +0800

    feat(EditShop): add value passing settings

--- apps/frontend/app/components/profile/ShopInfo.tsx
+++ apps/frontend/app/components/profile/ShopInfo.tsx
@@ -7,45 +7,64 @@ import TextArea from '@/components/Input/TextArea';
 
 type Props = {
   avatar: File;
+  avatarUrl?: string;
+  name: string;
+  phone: string;
+  address: string;
+  info: string;
   setName: React.Dispatch<React.SetStateAction<string>>;
   setPhone: React.Dispatch<React.SetStateAction<string>>;
   setAvatar: React.Dispatch<React.SetStateAction<File>>;
+  setOriginAvatar?: React.Dispatch<React.SetStateAction<string>>;
   setAddress: React.Dispatch<React.SetStateAction<string>>;
   setInfo: React.Dispatch<React.SetStateAction<string>>;
-  registerShop: () => Promise<void>;
+  buttonActionText: string;
+  handleEdit: () => Promise<void>;
 };
 
-const RegisterShopInfo = ({
+const EditShopInfoModal = ({
   avatar,
+  avatarUrl,
+  name,
+  phone,
+  address,
+  info,
   setName,
   setPhone,
   setAvatar,
+  setOriginAvatar,
   setAddress,
   setInfo,
-  registerShop,
+  buttonActionText,
+  handleEdit,
 }: Props) => {
   return (
     <div className="w-full max-w-lg m-auto max-[450px]:max-w-[350px] ">
       <PageTitle title="Set Information" />
       <RegisterBasicInfo
         avatar={avatar}
+        avatarUrl={avatarUrl}
+        name={name}
+        phone={phone}
         setName={setName}
         setPhone={setPhone}
         setAvatar={setAvatar}
+        setOriginAvatar={setOriginAvatar}
       />
       <InputModal
         topLabelText="ADDRESS"
         sideLabel={<FaStore />}
+        value={address}
         setValue={setAddress}
       />
       <div className="text-sm font-semibold ml-1 my-2">INFO</div>
-      <TextArea postBody="" setPostBody={setInfo} />
+      <TextArea postBody={info} setPostBody={setInfo} />
       <div className="w-full h-24" />
-      <BottomButton onClick={registerShop}>
-        <span>Create Account</span>
+      <BottomButton onClick={handleEdit}>
+        <span>{buttonActionText}</span>
       </BottomButton>
     </div>
   );
 };
 
-export default RegisterShopInfo;
+export default EditShopInfoModal;
