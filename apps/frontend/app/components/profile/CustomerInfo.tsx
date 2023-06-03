a8d602d508f9b219ba560b55f41fd3bc0631c9f8
dongdong867 <dongdong0867@gmail.com>
2023-06-04 06:05:40 +0800

    feat(EditCustomer): update value passing settings

--- apps/frontend/app/components/profile/CustomerInfo.tsx
+++ apps/frontend/app/components/profile/CustomerInfo.tsx
@@ -6,41 +6,58 @@ import BottomButton from '@/components/Button/BottomButton';
 
 type Props = {
   avatar: File;
+  avatarUrl?: string;
+  name: string;
+  phone: string;
+  email: string;
   setName: React.Dispatch<React.SetStateAction<string>>;
   setPhone: React.Dispatch<React.SetStateAction<string>>;
-  setAvatar: React.Dispatch<React.SetStateAction<File>>;
   setEmail: React.Dispatch<React.SetStateAction<string>>;
-  registerCustomer: () => Promise<void>;
+  setAvatar: React.Dispatch<React.SetStateAction<File>>;
+  setOriginAvatar?: React.Dispatch<React.SetStateAction<string>>;
+  buttonActionText: string;
+  handleEdit: () => Promise<void>;
 };
 
-const RegisterCustomerInfo = ({
+const EditCustomerInfoModal = ({
   avatar,
+  avatarUrl,
+  name,
+  phone,
+  email,
+  buttonActionText,
   setName,
   setEmail,
   setPhone,
+  setOriginAvatar,
   setAvatar,
-  registerCustomer,
+  handleEdit,
 }: Props) => {
   return (
     <div className="w-full max-w-lg h-full m-auto max-[450px]:max-w-[350px] ">
       <PageTitle title="Set Information" />
       <RegisterBasicInfo
         avatar={avatar}
+        avatarUrl={avatarUrl}
+        name={name}
+        phone={phone}
         setName={setName}
         setPhone={setPhone}
+        setOriginAvatar={setOriginAvatar}
         setAvatar={setAvatar}
       />
       <InputModal
         topLabelText="EMAIL"
         sideLabel={<MdEmail />}
+        value={email}
         setValue={setEmail}
       />
       <div className="w-full h-24" />
-      <BottomButton onClick={registerCustomer}>
-        <span>Create Account</span>
+      <BottomButton onClick={handleEdit}>
+        <span>{buttonActionText}</span>
       </BottomButton>
     </div>
   );
 };
 
-export default RegisterCustomerInfo;
+export default EditCustomerInfoModal;
