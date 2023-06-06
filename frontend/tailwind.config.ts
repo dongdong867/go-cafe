89e29e9f87383b78f5c5d93b28150a60932fe46b
dongdong867 <dongdong0867@gmail.com>
2023-06-07 00:26:19 +0800

    feat(tailwind): remove @nx imports

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,31 +1,29 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+const { join } = require("path");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     join(
       __dirname,
-      '{app,components,pages}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      "{app,components,pages}/**/*!(*.stories|*.spec).{ts,tsx,html}"
     ),
-    ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-Montserrat)'],
-        serif: ['var(--font-Montserrat)'],
-        mono: ['var(--font-Montserrat)'],
-        display: ['var(--font-Montserrat)'],
-        body: ['var(--font-Montserrat)'],
+        sans: ["var(--font-Montserrat)"],
+        serif: ["var(--font-Montserrat)"],
+        mono: ["var(--font-Montserrat)"],
+        display: ["var(--font-Montserrat)"],
+        body: ["var(--font-Montserrat)"],
       },
       boxShadow: {
-        basic: '0px 2px 5px rgba(0,0,0,0.25)',
+        basic: "0px 2px 5px rgba(0,0,0,0.25)",
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
   daisyui: {
-    themes: ['cupcake'],
+    themes: ["cupcake"],
   },
 };
