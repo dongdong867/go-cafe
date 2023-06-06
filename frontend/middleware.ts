2036ccfd41a4e1429c50611a604ae40140d6cd27
dongdong867 <dongdong0867@gmail.com>
2023-06-07 07:17:05 +0800

    fix(middleware): fix req test url

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,14 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = (req: NextRequest, res: NextResponse) => {
-  const token = req.cookies.get('token');
+  const token = req.cookies.get("token");
   if (token === undefined) {
     if (
-      req.url === 'http://localhost:4200/login' ||
-      req.url.startsWith('http://localhost:4200/_next')
+      req.url === "http://localhost:3000/login" ||
+      req.url.startsWith("http://localhost:3000/_next")
     )
       return NextResponse.next();
-    return NextResponse.redirect(new URL('/login', req.url));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
   return NextResponse.next();
 };
