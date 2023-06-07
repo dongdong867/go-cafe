6703833374035091e05478f6ff75308f384c2b95
dongdong867 <dongdong0867@gmail.com>
2023-06-07 17:55:16 +0800

    style(Login): prettify code

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -6,7 +6,6 @@ import RegisterModal from "./components/Modal/RegisterModal";
 
 const LoginPage = () => {
   localStorage.clear();
-
   const cookies = document.cookie.split(";");
   for (const cookie of cookies) {
     document.cookie = cookie + "=; expires=" + new Date(0).toUTCString();
