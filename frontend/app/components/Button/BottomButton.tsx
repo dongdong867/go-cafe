de05ece380a7589bd37c31bd5217adad796bdb14
dongdong867 <dongdong0867@gmail.com>
2023-06-07 08:47:38 +0800

    feat(BottomButton): add z-index setting

--- frontend/app/components/Button/BottomButton.tsx
+++ frontend/app/components/Button/BottomButton.tsx
@@ -6,7 +6,7 @@ type Props = {
 const BottomButton = ({ children, onClick }: Props) => {
   return (
     <>
-      <div className="fixed w-full bg-base-100 left-0 bottom-0">
+      <div className="fixed w-full bg-base-100 left-0 bottom-0 z-50">
         <div className="w-11/12 max-w-lg m-auto py-4">
           <button
             onClick={onClick}
