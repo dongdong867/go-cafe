2436a85cfd0058dfe8fa9e0a32f4944908f26f91
dongdong867 <dongdong0867@gmail.com>
2023-06-09 21:19:41 +0800

    fix(ShopRating): fix rating justify setting

--- frontend/app/components/ShopRatingModal.tsx
+++ frontend/app/components/ShopRatingModal.tsx
@@ -8,13 +8,13 @@ const ShopRatingModal = ({ rateName, rateValue }: Props) => {
     <>
       <div className="w-11/12 min-[450px]:flex justify-between place-items-center space-x-2">
         <div className="w-1/3 max-[450px]:w-full">{rateName}</div>
-        <div className="w-2/3 flex place-items-center px-2 space-x-2 max-[450px]:w-full">
+        <div className="w-2/3 flex justify-between place-items-center px-2 space-x-2 max-[450px]:w-full">
           <progress
             value={rateValue}
             max={5}
             className="progress progress-accent w-full"
           />
-          <div className="w-max">{rateValue.toFixed(2)}</div>
+          <div className="w-1/12">{rateValue.toFixed(2)}</div>
         </div>
       </div>
     </>
