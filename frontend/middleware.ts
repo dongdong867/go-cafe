6ae26c458930a687af399753841d45d39f0ef075
dongdong867 <dongdong0867@gmail.com>
2023-06-12 03:40:54 +0800

    feat(middleware): update domain

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -4,8 +4,8 @@ export const middleware = (req: NextRequest, res: NextResponse) => {
   const token = req.cookies.get("token");
   if (token === undefined) {
     if (
-      req.url === "https://go-cafe.vercel.app/login" ||
-      req.url.startsWith("https://go-cafe.vercel.app/_next")
+      req.url === "https://gocafe.vercel.app/login" ||
+      req.url.startsWith("https://gocafe.vercel.app/_next")
     )
       return NextResponse.next();
     return NextResponse.redirect(new URL("/login", req.url));
