88cf332a854f7ea39f5228373c14884207563926
dongdong867 <dongdong0867@gmail.com>
2023-06-12 05:01:32 +0800

    feat(SearchBarLoading): create Loading

---
+++ frontend/app/(home)/components/SearchBarLoading.tsx
@@ -0,0 +1,10 @@
+const SearchBarLoading = () => {
+  return (
+    <div className="flex space-x-2 py-2">
+      <div className="loading loading-dots" />
+      <div className="font-bold">Search Bar Preparing</div>
+    </div>
+  );
+};
+
+export default SearchBarLoading;
