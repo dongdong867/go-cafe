07b606f0bcd9e3fec23af68f1ccac1194f14cbdf
dongdong867 <dongdong0867@gmail.com>
2023-06-12 09:28:53 +0800

    feat(SearchBarLoading): change component to client

--- frontend/app/(home)/components/SearchBarLoading.tsx
+++ frontend/app/(home)/components/SearchBarLoading.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 const SearchBarLoading = () => {
   return (
-    <div className="flex space-x-2 py-2">
-      <div className="loading loading-dots" />
-      <div className="font-bold">Search Bar Preparing</div>
+    <div className="w-full h-20 flex justify-center place-items-center space-x-4">
+      <div className="loading loading-spinner loading-lg text-primary" />
+      <div className="font-bold text-xl">Searching...</div>
     </div>
   );
 };
