c43d5c9052de8954f076b5200cb85dee395948db
dongdong867 <dongdong0867@gmail.com>
2023-06-13 02:06:41 +0800

    feat(UserProfile): add graphql query for loading

--- frontend/app/user/components/CustomerProfile.tsx
+++ frontend/app/user/components/CustomerProfile.tsx
@@ -1,17 +1,52 @@
+import { getClient } from "@/lib/client";
+import { gql } from "@apollo/client";
 import Image from "next/image";
 
-type Props = {
-  data: User;
+type GraphQLType = {
+  customer: {
+    user: {
+      avatar: {
+        data: string;
+      };
+      account: string;
+      name: string;
+      phone: string;
+      postCount: number;
+    };
+    email: string;
+    followingCount: number;
+  };
 };
 
-const UserCustomerProfile = ({ data }: Props) => {
+const query = gql`
+  query Customer {
+    customer {
+      user {
+        avatar {
+          data
+        }
+        account
+        name
+        phone
+        postCount
+      }
+      email
+      followingCount
+    }
+  }
+`;
+
+const UserCustomerProfile = async () => {
+  const client = getClient();
+  const { data }: { data: GraphQLType } = await client.query({ query });
+
   return (
     <>
       <div className="w-full flex justify-around place-items-center pt-4">
         <div className="avatar">
           <div className="w-40 h-40 max-[450px]:w-24 max-[450px]:h-24 rounded-full">
             <Image
-              src={data.user.avatar.data}
+              src={data.customer.user.avatar.data}
               alt=""
               width={500}
               height={500}
@@ -22,17 +57,17 @@ const UserCustomerProfile = ({ data }: Props) => {
 
         <div className="space-y-4">
           <div className="text-4xl max-[450px]:text-3xl font-bold">
-            {data.user.name}
+            {data.customer.user.name}
           </div>
           <div className="flex text-xl max-[450px]:text-lg font-semibold">
             <div>
               <div>Posts</div>
-              <div>{data.user.postCount}</div>
+              <div>{data.customer.user.postCount}</div>
             </div>
             <div className="divider divider-horizontal" />
             <div>
               <div>Following</div>
-              <div>{data.followingCount}</div>
+              <div>{data.customer.followingCount}</div>
             </div>
           </div>
         </div>
