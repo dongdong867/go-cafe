9c7ba0f7050ec72ee89460fccac7aee0d8bd3ce8
dongdong867 <dongdong0867@gmail.com>
2023-06-13 02:07:07 +0800

    feat(User): add suspense settings

--- frontend/app/user/page.tsx
+++ frontend/app/user/page.tsx
@@ -8,71 +8,22 @@ import Loading from "../components/Loading/Loading";
 import UserPosts from "./components/UserPosts";
 import ShopPosts from "./components/ShopPosts";
 
-const customerQuery = gql`
-  query Customer {
-    customer {
-      user {
-        avatar {
-          data
-        }
-        account
-        name
-        phone
-        postCount
-      }
-      email
-      followingCount
-    }
-  }
-`;
-
-const shopQuery = gql`
-  query Self {
-    storeSelf {
-      user {
-        avatar {
-          data
-        }
-        account
-        name
-        phone
-        postCount
-      }
-      address
-      info
-      storeRating {
-        postCount
-        rating {
-          general
-          environment
-          meals
-          attitude
-        }
-      }
-    }
-  }
-`;
-
 const UserPage = async () => {
   const role = cookies().get("role")?.value;
 
-  const client = getClient();
-  const { data } =
-    role === "customer"
-      ? await client.query({ query: customerQuery })
-      : await client.query({ query: shopQuery });
-
   const Info = () => {
     if (role === "customer") {
-      return <CustomerProfile data={data.customer} />;
+      return <CustomerProfile />;
     } else {
-      return <UserShopProfile data={data.storeSelf} />;
+      return <UserShopProfile />;
     }
   };
 
   return (
     <div className="w-full h-fit max-w-lg max-[450px]:w-11/12 m-auto">
-      <Info />
+      <Suspense fallback={<Loading />}>
+        <Info />
+      </Suspense>
       <Suspense fallback={<Loading />}>
         {role === "customer" ? await UserPosts() : await ShopPosts()}
       </Suspense>
