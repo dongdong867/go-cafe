fed80c73779c27c5d374cf566312fbbdf1c14589
dongdong867 <dongdong0867@gmail.com>
2023-06-13 04:14:24 +0800

    feat(About): add suspense settings

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,29 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "public/images/logo.png";
+import { Suspense } from "react";
+import Loading from "../components/Loading/Loading";
 
 const AboutPage = () => {
   return (
-    <div className="w-full max-w-lg min-[450px]:w-11/12 h-[calc(100vh-120px)] m-auto flex flex-col justify-center place-items-center">
-      <Image src={Logo} alt="" className="w-40 aspect-square" />
-      <div className="text-2xl font-semibold text-center">
-        <div className="text-4xl font-bold">Go Cafe</div>
-        <div className="text-sm">version 1.0.0 (2023/06)</div>
-        <div className="text-xs mt-4">
-          <div className="space-x-1">
-            <span>Copyright © 2023</span>
-            <Link
-              target="_blank"
-              href={"https://github.com/dongdong867"}
-              className="text-primary underline underline-offset-1"
-            >
-              Dong
-            </Link>
+    <Suspense fallback={<Loading />}>
+      <div className="w-full max-w-lg min-[450px]:w-11/12 h-[calc(100vh-120px)] m-auto flex flex-col justify-center place-items-center">
+        <Image src={Logo} alt="" className="w-40 aspect-square" />
+        <div className="text-2xl font-semibold text-center">
+          <div className="text-4xl font-bold">Go Cafe</div>
+          <div className="text-sm">version 1.0.0 (2023/06)</div>
+          <div className="text-xs mt-4">
+            <div className="space-x-1">
+              <span>Copyright © 2023</span>
+              <Link
+                target="_blank"
+                href={"https://github.com/dongdong867"}
+                className="text-primary underline underline-offset-1"
+              >
+                Dong
+              </Link>
+            </div>
+            <div>All rights reserved.</div>
           </div>
-          <div>All rights reserved.</div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
