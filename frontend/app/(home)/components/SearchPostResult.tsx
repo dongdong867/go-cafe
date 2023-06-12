aa001b9db17301e76166d33a273b74008d17dac8
dongdong867 <dongdong0867@gmail.com>
2023-06-13 01:12:16 +0800

    feat(SearchPostResult): add not found handler

--- frontend/app/(home)/components/SearchPostResult.tsx
+++ frontend/app/(home)/components/SearchPostResult.tsx
@@ -1,6 +1,7 @@
 import PageTitle from "@/app/components/PageTitle";
 import UserPostModal from "@/app/components/UserPostModal";
 import { ApolloError, gql, useSuspenseQuery } from "@apollo/client";
+import Link from "next/link";
 import { useDeferredValue } from "react";
 import toast from "react-hot-toast";
 
@@ -62,17 +63,36 @@ const SearchPostResult = ({ searchQuery }: Props) => {
 
   const deferredCustomerAtStore = useDeferredValue(data.customerPostAtStore);
 
-  return (
-    deferredCustomerAtStore.length > 0 && (
-      <div className="pb-4">
-        {deferredCustomerAtStore.map((customerPost) => (
-          <UserPostModal key={customerPost.id} customerPost={customerPost} />
-        ))}
-        <div className="w-full max-[450px]:w-11/12 m-auto">
-          <PageTitle title="Recommend posts" />
+  return deferredCustomerAtStore.length > 0 ? (
+    <div className="pb-4">
+      {deferredCustomerAtStore.map((customerPost) => (
+        <UserPostModal key={customerPost.id} customerPost={customerPost} />
+      ))}
+      <div className="divider divider-vertical w-full my-2 m-auto" />
+      <div className="w-full max-[450px]:w-11/12 m-auto">
+        <div className="text-xl max-[450px]:text-lg mb-4 font-bold">
+          Recommended Posts:
         </div>
       </div>
-    )
+    </div>
+  ) : (
+    <div className="w-11/12 max-w-lg font-bold">
+      <div className="w-full pt-4 space-y-2">
+        <div className="w-full break-normal text-2xl max-[450px]:text-xl">
+          No post found at
+        </div>
+        <div className="w-full flex justify-center text-3xl max-[450px]:text-2xl">
+          <Link
+            href={`/shop/${searchQuery}`}
+            className="text-primary underline underline-offset-2"
+          >
+            @{searchQuery}
+          </Link>
+        </div>
+      </div>
+      <div className="divider divider-vertical w-full my-2 m-auto" />
+      <div className="text-xl max-[450px]:text-lg mb-4">Recommended Posts:</div>
+    </div>
   );
 };
 
