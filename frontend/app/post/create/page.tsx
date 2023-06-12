5006e3d6168c5e9bd0bb2153a3c00c82004d4fec
dongdong867 <dongdong0867@gmail.com>
2023-06-12 11:46:32 +0800

    feat(CreatePost): add suspense settings

--- frontend/app/post/create/page.tsx
+++ frontend/app/post/create/page.tsx
@@ -1,39 +1,22 @@
-"use client";
-
 import PageTitle from "@/app/components/PageTitle";
-import useCreatePost from "@/app/hooks/useCreatePost";
 import EditModal from "../components/EditModal/Modal";
+import { Suspense } from "react";
+import EditPostLoading from "../components/EditPostLoading";
 
 const CreatePostPage = () => {
-  const {
-    pictureList,
-    setShopAccount,
-    setRate,
-    setBody,
-    setPictureList,
-    createPost,
-  } = useCreatePost();
-
   return (
-    <>
-      <div
-        className="
+    <div
+      className="
           w-full max-w-lg 
           h-[calc(100vh-64px)] 
           max-[450px]:max-w-[350px] 
           m-auto"
-      >
-        <PageTitle title="Create Post" />
-        <EditModal
-          pictureList={pictureList}
-          setShopAccount={setShopAccount}
-          setRate={setRate}
-          setPostBody={setBody}
-          setPictureList={setPictureList}
-          onSubmit={createPost}
-        />
-      </div>
-    </>
+    >
+      <PageTitle title="Create Post" />
+      <Suspense fallback={<EditPostLoading />}>
+        <EditModal />
+      </Suspense>
+    </div>
   );
 };
 
