c7707954b426bba814c0dbee82882fd6ae48d67d
dongdong867 <dongdong0867@gmail.com>
2023-06-12 12:20:03 +0800

    feat(EditShopPost): move graphql for loading

--- frontend/app/post/components/ShopPostModal.tsx
+++ frontend/app/post/components/ShopPostModal.tsx
@@ -5,32 +5,77 @@ import EditPicture from "./EditModal/Picture";
 import { BsHash } from "react-icons/bs";
 import TextArea from "@/app/components/Input/TextArea";
 import BottomButton from "@/app/components/Button/BottomButton";
+import useCreateShopPost from "@/app/hooks/useCreateShopPost";
+import useUpdateShopPost from "@/app/hooks/useUpdateShopPost";
 
-type Props = {
-  title: string;
-  body: string;
-  pictureList: File[];
-  originPicture?: string[];
-  deletedPicture?: string[];
-  setTitle: React.Dispatch<React.SetStateAction<string>>;
-  setBody: React.Dispatch<React.SetStateAction<string>>;
-  setPictureList: React.Dispatch<React.SetStateAction<File[]>>;
-  setDeletedPicture?: React.Dispatch<React.SetStateAction<string[]>>;
-  onSubmit: () => void;
+const getPostData = (postId: string | undefined) => {
+  if (!postId) {
+    const {
+      title,
+      body,
+      pictureList,
+      setTitle,
+      setBody,
+      setPictureList,
+      createPost: onSubmit,
+    } = useCreateShopPost();
+
+    return {
+      title,
+      body,
+      pictureList,
+      setTitle,
+      setBody,
+      setPictureList,
+      onSubmit,
+    };
+  } else {
+    const {
+      title,
+      body,
+      originPicture,
+      addedPicture: pictureList,
+      deletedPicture,
+      setTitle,
+      setBody,
+      setAddedPicture: setPictureList,
+      setDeletedPicture,
+      updatePost: onSubmit,
+    } = useUpdateShopPost(postId);
+
+    return {
+      title,
+      body,
+      originPicture,
+      pictureList,
+      deletedPicture,
+      setTitle,
+      setBody,
+      setPictureList,
+      setDeletedPicture,
+      onSubmit,
+    };
+  }
 };
 
-const EditShopPostModal = ({
-  title,
-  body,
-  pictureList,
-  originPicture = [],
-  deletedPicture = [],
-  setTitle,
-  setBody,
-  setPictureList,
-  setDeletedPicture = undefined,
-  onSubmit,
-}: Props) => {
+type Props = {
+  postId?: string;
+};
+
+const EditShopPostModal = ({ postId = undefined }: Props) => {
+  const {
+    title,
+    body,
+    pictureList,
+    originPicture = [],
+    deletedPicture = [],
+    setTitle,
+    setBody,
+    setPictureList,
+    setDeletedPicture = undefined,
+    onSubmit,
+  } = getPostData(postId);
+
   return (
     <>
       <div>
