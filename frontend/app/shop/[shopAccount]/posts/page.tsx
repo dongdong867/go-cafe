6318be96b120ab6c36309229c7b0d650d3c90311
dongdong867 <dongdong0867@gmail.com>
2023-06-13 00:00:16 +0800

    feat(UserPostAtShop): add suspense settings

--- frontend/app/shop/[shopAccount]/posts/page.tsx
+++ frontend/app/shop/[shopAccount]/posts/page.tsx
@@ -1,72 +1,6 @@
-import PageTitle from "@/app/components/PageTitle";
-import UserPostModal from "@/app/components/UserPostModal";
-import { getClient } from "@/lib/client";
-import { gql } from "@apollo/client";
-
-type GraphQLType = {
-  customerPostAtStore: {
-    id: string;
-    post: {
-      body: string;
-      postPicture: {
-        picture: {
-          data: string;
-        };
-      }[];
-    };
-    rating: {
-      general: number;
-      environment: number;
-      meals: number;
-      attitude: number;
-    };
-    store: {
-      user: {
-        account: string;
-        name: string;
-      };
-    };
-    customer: {
-      user: {
-        account: string;
-        name: string;
-      };
-    };
-  }[];
-};
-
-const query = gql`
-  query GetCustomerPostAtStore($storeAccount: String!) {
-    customerPostAtStore(storeAccount: $storeAccount) {
-      id
-      post {
-        body
-        postPicture {
-          picture {
-            data
-          }
-        }
-      }
-      rating {
-        general
-        environment
-        meals
-        attitude
-      }
-      store {
-        user {
-          account
-          name
-        }
-      }
-      customer {
-        user {
-          name
-        }
-      }
-    }
-  }
-`;
+import UserPostsAtStore from "./components/UserPostsAtStore";
+import { Suspense } from "react";
+import Loading from "@/app/components/Loading/Loading";
 
 type Props = {
   params: {
@@ -75,25 +9,16 @@ type Props = {
 };
 
 const ShopPostsPage = async ({ params }: Props) => {
-  const client = getClient();
-  const { data }: { data: GraphQLType } = await client.query({
-    query,
-    variables: {
-      storeAccount: decodeURIComponent(params.shopAccount),
-    },
-  });
-
   return (
-    <>
-      <div className="w-full max-w-lg max-[450px]:w-11/12 m-auto">
-        <PageTitle
-          title={`Post at @${decodeURIComponent(params.shopAccount)}`}
-        />
-        {data.customerPostAtStore.map((post) => (
-          <UserPostModal key={post.id} customerPost={post} />
-        ))}
-      </div>
-    </>
+    <div className="w-full max-w-lg font-bold  max-[450px]:w-11/12 m-auto">
+      <div className="text-3xl pt-4">Posts at</div>
+      <div className="text-2xl">@{params.shopAccount}</div>
+      <div className="divider" />
+
+      <Suspense fallback={<Loading />}>
+        {await UserPostsAtStore(params.shopAccount)}
+      </Suspense>
+    </div>
   );
 };
 
