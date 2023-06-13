cb9f782b31bf7d24eb20b7015861cdf45f070aa5
dongdong867 <dongdong0867@gmail.com>
2023-06-13 12:56:46 +0800

    fix(MenuLoading): fix loading display height

--- frontend/app/shop/[shopAccount]/menu/components/MenuLoading.tsx
+++ frontend/app/shop/[shopAccount]/menu/components/MenuLoading.tsx
@@ -1,6 +1,6 @@
 const MenuLoading = () => {
   return (
-    <div className="w-full h-[calc(100vh-270px)] flex justify-center place-items-center space-x-4">
+    <div className="w-full h-[calc(100vh-100px)] flex justify-center place-items-center space-x-4">
       <div className="loading loading-spinner loading-lg text-primary" />
       <div className="font-bold text-xl">Shopping Cart Loading...</div>
     </div>
