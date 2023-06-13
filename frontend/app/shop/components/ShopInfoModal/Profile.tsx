bbad1acf3b6fef9499ab076d54d6303b1c9d8603
dongdong867 <dongdong0867@gmail.com>
2023-06-13 14:09:13 +0800

    feat(ShopProfile): add links to shop profile page

--- frontend/app/shop/components/ShopInfoModal/Profile.tsx
+++ frontend/app/shop/components/ShopInfoModal/Profile.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type GraphQLType = {
   user: {
     account: string;
@@ -26,22 +28,30 @@ const ShopProfile = ({ data }: Props) => {
     <>
       <div className="bg-base-100 p-4 rounded-xl space-y-4">
         <div className="card-title text-2xl font-bold">{data.user.name}</div>
-        <div className="font-medium space-y-2">
+        <div className="font-semibold space-y-2">
           <div className="text-base">
             <div className="flex space-x-1">
               <div>Tel: </div>
-              <div className="font-semibold">{data.user.phone}</div>
+              <Link
+                href={`tel:${data.user.phone}`}
+                className="text-primary-focus underline underline-offset-2"
+              >
+                {data.user.phone}
+              </Link>
             </div>
             <div className="space-x-1">
               <div>Info: </div>
-              <div className="font-semibold whitespace-pre-line">
-                {data.info}
-              </div>
+              <div className=" whitespace-pre-line">{data.info}</div>
             </div>
             <div className="mt-4">
               <div>Address: </div>
-              <div className="flex gap-x-1 flex-wrap font-semibold">
-                <div className="shrink-0">{data.address}</div>
+              <div className="flex gap-x-1 flex-wrap">
+                <Link
+                  href={`https://www.google.com/maps/place/${data.address}`}
+                  className="text-primary-focus underline underline-offset-2"
+                >
+                  {data.address}
+                </Link>
               </div>
             </div>
           </div>
