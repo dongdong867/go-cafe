2e0e099db39bbd4f95040a9ae951c43d9314a100
dongdong867 <dongdong0867@gmail.com>
2023-06-13 12:49:06 +0800

    fix(MenuLoading): update loading describe text

--- frontend/app/shop/[shopAccount]/menu/components/MenuLoading.tsx
+++ frontend/app/shop/[shopAccount]/menu/components/MenuLoading.tsx
@@ -2,7 +2,7 @@ const MenuLoading = () => {
   return (
     <div className="w-full h-[calc(100vh-270px)] flex justify-center place-items-center space-x-4">
       <div className="loading loading-spinner loading-lg text-primary" />
-      <div className="font-bold text-xl">Adding to cart...</div>
+      <div className="font-bold text-xl">Shopping Cart Loading...</div>
     </div>
   );
 };
