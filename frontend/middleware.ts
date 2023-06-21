d308458061b8a76025d0529ba8a7b02294f7954e
dongdong867 <dongdong0867@gmail.com>
2023-06-22 02:15:09 +0800

    feat(middleware): add sites to middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -5,7 +5,11 @@ export const middleware = (req: NextRequest, res: NextResponse) => {
   if (token === undefined) {
     if (
       req.url === "https://gocafe.vercel.app/login" ||
-      req.url.startsWith("https://gocafe.vercel.app/_next")
+      req.url === "https://dongdong867.dev/login" ||
+      req.url === "https://www.dongdong867.dev/login" ||
+      req.url.startsWith("https://gocafe.vercel.app/_next") ||
+      req.url.startsWith("https://dongdong867.dev/_next") ||
+      req.url.startsWith("https://www.dongdong867.dev/_next")
     )
       return NextResponse.next();
     return NextResponse.redirect(new URL("/login", req.url));
